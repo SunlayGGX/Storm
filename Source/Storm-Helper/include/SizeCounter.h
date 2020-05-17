6b3e49a1e57fd1b8a03dea4714697acb69fffe44
SunlayGGX <sunlay.g@free.fr>
2020-05-17 13:51:58 -0400

    Implemented SizeCounter.

--- Source/Storm-Helper/include/SizeCounter.h
+++ Source/Storm-Helper/include/SizeCounter.h
@@ -1,1 +1,25 @@
 #pragma once
+
+
+namespace Storm
+{
+    template<class Type, class ... Others>
+    struct SizeCounter
+    {
+    public:
+        enum
+        {
+            value = SizeCounter<Type>::value + SizeCounter<Others...>::value
+        };
+    };
+
+    template<class Type>
+    struct SizeCounter<Type>
+    {
+    public:
+        enum
+        {
+            value = sizeof(Type)
+        };
+    };
+}
