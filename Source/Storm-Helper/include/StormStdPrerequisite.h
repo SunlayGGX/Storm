b594003a0e0300e2c375ed22e3f8cf7f2653e0e6
SunlayGGX <sunlay.g@free.fr>
2020-05-17 16:05:11 -0400

    Add a base std prerequisite to be shared inside all pch. This is what I'm commonly use...

---
+++ Source/Storm-Helper/include/StormStdPrerequisite.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// Base
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <exception>
+#include <cassert>
+
+// Container
+#include <vector>
+#include <map>
+
+// Threading
+#include <atomic>
+#include <mutex>
+#include <thread>
