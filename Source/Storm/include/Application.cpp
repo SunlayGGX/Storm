adb0e7a59f333e653fb39aa0a4b67f3b5e0c08ea
SunlayGGX <sunlay.g@free.fr>
2020-05-17 18:37:56 -0400

    Use the Singleton Allocator.

--- Source/Storm/include/Application.cpp
+++ Source/Storm/include/Application.cpp
@@ -1,14 +1,25 @@
 #include "Application.h"
 
+#include "SingletonAllocator.h"
+#include "SingletonHolder.h"
+
+namespace
+{
+    using SingletonAllocatorAlias = Storm::SingletonAllocator<
+        Storm::SingletonHolder
+    >;
+
+    std::unique_ptr<SingletonAllocatorAlias> g_singletonMaker;
+}
 
 Storm::Application::Application(int argc, const char* argv[])
 {
-
+    g_singletonMaker = std::make_unique<SingletonAllocatorAlias>();
 }
 
 Storm::Application::~Application()
 {
-
+    g_singletonMaker.reset();
 }
 
 Storm::ExitCode Storm::Application::run()
