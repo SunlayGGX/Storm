29574ef224ee810025c72795ae0096d95766eb1c
SunlayGGX <sunlay.g@free.fr>
2020-05-17 22:02:42 -0400

    Manual Printing when we leave the application with an exception.

--- Source/Storm/include/Storm.cpp
+++ Source/Storm/include/Storm.cpp
@@ -1,15 +1,19 @@
 #include "Application.h"
 
+#include <iostream>
+
 
 int main(int argc, const char* argv[]) try
 {
     return static_cast<int>(Storm::Application{ argc, argv }.run());
 }
-catch (const std::exception &/*ex*/)
+catch (const std::exception &ex)
 {
+    std::cerr << "Fatal error (std::exception received) : " << ex.what() << '\n';
     return static_cast<int>(Storm::ExitCode::k_stdException);
 }
 catch (...)
 {
+    std::cerr << "Fatal error (unknown exception received)\n";
     return static_cast<int>(Storm::ExitCode::k_unknownException);
 }
