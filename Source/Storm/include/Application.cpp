a05f3dab92ec205f4bef53674f137b7c9031a6f9
SunlayGGX <sunlay.g@free.fr>
2020-05-17 22:55:57 -0400

    Initialize the LoggerManager and the ConfigManager.

--- Source/Storm/include/Application.cpp
+++ Source/Storm/include/Application.cpp
@@ -2,11 +2,15 @@
 
 #include "SingletonAllocator.h"
 #include "SingletonHolder.h"
+#include "LoggerManager.h"
+#include "ConfigManager.h"
 
 namespace
 {
     using SingletonAllocatorAlias = Storm::SingletonAllocator<
-        Storm::SingletonHolder
+        Storm::SingletonHolder,
+        Storm::ConfigManager,
+        Storm::LoggerManager
     >;
 
     std::unique_ptr<SingletonAllocatorAlias> g_singletonMaker;
@@ -15,14 +19,28 @@ namespace
 Storm::Application::Application(int argc, const char* argv[])
 {
     g_singletonMaker = std::make_unique<SingletonAllocatorAlias>();
+
+    LOG_COMMENT << "Application Creation started";
+
+    Storm::ConfigManager::instance().initialize(argc, argv);
+    Storm::LoggerManager::instance().initialize();
+
+    LOG_COMMENT << "Application Creation finished";
 }
 
 Storm::Application::~Application()
 {
+    LOG_COMMENT << "Application Cleanup";
+
+    Storm::ConfigManager::instance().cleanUp();
+    Storm::LoggerManager::instance().cleanUp();
+
     g_singletonMaker.reset();
 }
 
 Storm::ExitCode Storm::Application::run()
 {
+    LOG_COMMENT << "Application Run started";
+
     return ExitCode::k_success;
 }
