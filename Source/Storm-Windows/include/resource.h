b4d05db3b2aac8bdc29a2e59607944c88077e15b
SunlayGGX <sunlay.g@free.fr>
2020-05-18 09:08:19 -0400

    Add some windows Ressources.

---
+++ Source/Storm-Windows/include/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// fichier Include Microsoft Visual C++.
+// Utilisé par Storm-Windows.rc
+//
+#define IDI_SMALL                       102
+#define IDS_STORM                		103
+#define IDS_TITLE                       104
+#define IDR_ACCELERATOR1                104
+#define IDR_MENU1                       105
+#define IDR_STORM                		105
+#define IDR_STORM               		105
+#define IDI_STORM               		40005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
