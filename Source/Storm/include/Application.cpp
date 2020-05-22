1876284c012d1db5a6ca0ab6b8870989c7b9769e
SunlayGGX <sunlay.g@free.fr>
2020-05-22 08:33:13 -0400

    Normalize tabulations.

--- Source/Storm/include/Application.cpp
+++ Source/Storm/include/Application.cpp
@@ -13,8 +13,8 @@ namespace
     using SingletonAllocatorAlias = Storm::SingletonAllocator<
         Storm::SingletonHolder,
         Storm::ConfigManager,
-		Storm::LoggerManager,
-		Storm::InputManager,
+        Storm::LoggerManager,
+        Storm::InputManager,
         Storm::WindowsManager,
         Storm::GraphicManager
     >;
@@ -29,12 +29,12 @@ Storm::Application::Application(int argc, const char* argv[])
     LOG_COMMENT << "Application Creation started";
 
     Storm::ConfigManager::instance().initialize(argc, argv);
-	Storm::LoggerManager::instance().initialize();
+    Storm::LoggerManager::instance().initialize();
 
-	Storm::InputManager::instance().initialize();
+    Storm::InputManager::instance().initialize();
 
-	Storm::GraphicManager::instance().initialize();
-	Storm::WindowsManager::instance().initialize();
+    Storm::GraphicManager::instance().initialize();
+    Storm::WindowsManager::instance().initialize();
 
     LOG_COMMENT << "Application Creation finished";
 }
@@ -43,10 +43,10 @@ Storm::Application::~Application()
 {
     LOG_COMMENT << "Application Cleanup";
 
-	Storm::InputManager::instance().cleanUp();
-	Storm::GraphicManager::instance().cleanUp();
-	Storm::WindowsManager::instance().cleanUp();
-	Storm::ConfigManager::instance().cleanUp();
+    Storm::InputManager::instance().cleanUp();
+    Storm::GraphicManager::instance().cleanUp();
+    Storm::WindowsManager::instance().cleanUp();
+    Storm::ConfigManager::instance().cleanUp();
     Storm::LoggerManager::instance().cleanUp();
 
     g_singletonMaker.reset();
@@ -55,11 +55,11 @@ Storm::Application::~Application()
 Storm::ExitCode Storm::Application::run()
 {
     if (!Storm::ConfigManager::instance().shouldDisplayHelp())
-	{
-		LOG_COMMENT << "Creating Application Windows";
-		Storm::WindowsManager::instance().initialize();
+    {
+        LOG_COMMENT << "Creating Application Windows";
+        Storm::WindowsManager::instance().initialize();
 
-		LOG_COMMENT << "Start Application Run";
+        LOG_COMMENT << "Start Application Run";
         // TODO
     }
 
