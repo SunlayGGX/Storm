db5a3d02fc6c4b2d7dd5d55f5518550da6c9a0c4
SunlayGGX <sunlay.g@free.fr>
2020-05-22 12:13:47 -0400

    Fix setup script green even though junstion creation failed.

--- Build/Script/Internal/SetupImpl.bat
+++ Build/Script/Internal/SetupImpl.bat
@@ -22,8 +22,7 @@ set makeFolderLink=call :CreateFolderLink
 
 
 
-
-exit /B 0
+exit /B %errorlevel%
 
 
 :CreateFolderLink
@@ -31,14 +30,13 @@ echo "%~1" to "%~2"
 if not exist "%~2" (
 	echo Cannot create "%~1" because the target "%~2" doesn't exist. Aborting!
 	set errorlevel=90009
-	goto:eof
 )
 if exist "%~1" rmdir "%~1"
 mklink /J "%~1" "%~2"
 if errorlevel 1 (
 	echo Failed to create junction from "%~1" to "%~2" !
 	echo.
-	goto:eof
 )
 echo.
-exit /B 0
+exit /B errorlevel
+
