42a26e9f59c3fab142ae5c18af4419c637b6a729
SunlayGGX <sunlay.g@free.fr>
2020-05-23 09:20:35 -0400

    Add some settings to Camera object.

---
+++ Source/Storm-Graphics/include/Camera.cpp
@@ -0,0 +1,9 @@
+#include "Camera.h"
+
+
+Storm::Camera::Camera() :
+	_nearPlane{ 0.5f },
+	_farPlane{ 50.f }
+{
+
+}
--- Source/Storm-Graphics/include/Camera.h
+++ Source/Storm-Graphics/include/Camera.h
@@ -1,1 +1,14 @@
 #pragma once
+
+namespace Storm
+{
+	class Camera
+	{
+	public:
+		Camera();
+
+	private:
+		float _nearPlane;
+		float _farPlane;
+	};
+}
