eec77298036acf5d2f16e5ea5d384dbfdf681906
SunlayGGX <sunlay.g@free.fr>
2020-05-23 09:10:11 -0400

    Declare an alias to use ComPtr easily.

--- Source/Storm-Graphics/include/Storm-GraphicsPCH.h
+++ Source/Storm-Graphics/include/Storm-GraphicsPCH.h
@@ -1,7 +1,12 @@
 #pragma once
 
 
+#include <wrl/client.h>
+
 #include "StormStdPrerequisite.h"
 
 #include "Logging.h"
 #define STORM_MODULE_NAME "Graphics"
+
+
+template<class Type> using ComPtr = Microsoft::WRL::ComPtr<Type>;
