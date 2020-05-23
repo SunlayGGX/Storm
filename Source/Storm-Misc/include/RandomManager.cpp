420bdfcbf1b708ff8403b67ff19dc7b40f9c263b
SunlayGGX <sunlay.g@free.fr>
2020-05-23 13:28:09 -0400

    Implement the initialization of the RandomManager with seed saving and reloading.

--- Source/Storm-Misc/include/RandomManager.cpp
+++ Source/Storm-Misc/include/RandomManager.cpp
@@ -1,9 +1,20 @@
 #include "RandomManager.h"
+#include "SerializePackage.h"
 
 
-Storm::RandomManager::RandomManager()
+Storm::RandomManager::RandomManager() :
+	_seed{ static_cast<uint64_t>(time(nullptr)) }
 {
-
+	_randomEngine.seed(_seed);
 }
 
 Storm::RandomManager::~RandomManager() = default;
+
+void Storm::RandomManager::serialize(Storm::SerializePackage &package)
+{
+	package << _seed;
+	if (!package.isSerializing())
+	{
+		_randomEngine.seed(_seed);
+	}
+}
--- Source/Storm-Misc/include/RandomManager.h
+++ Source/Storm-Misc/include/RandomManager.h
@@ -2,14 +2,26 @@
 
 #include "Singleton.h"
 #include "IRandomManager.h"
+#include "SingletonDefaultImplementation.h"
+
+#include <random>
 
 
 namespace Storm
 {
+	class SerializePackage;
+
 	class RandomManager :
-		private Storm::Singleton<RandomManager>,
+		private Storm::Singleton<RandomManager, Storm::DefineDefaultInitAndCleanupImplementation>,
 		public Storm::IRandomManager
 	{
 		STORM_DECLARE_SINGLETON(RandomManager);
+
+	public:
+		void serialize(Storm::SerializePackage &package);
+
+	private:
+		uint64_t _seed;
+		std::mt19937_64 _randomEngine;
 	};
 }
