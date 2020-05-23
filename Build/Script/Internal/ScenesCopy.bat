2381db5a587ba6902b58a0e38f89a42fee47232f
SunlayGGX <sunlay.g@free.fr>
2020-05-23 09:08:22 -0400

    Fix the issue where the script scene copy failed when the target folder doesn't exist.

--- Build/Script/Internal/ScenesCopy.bat
+++ Build/Script/Internal/ScenesCopy.bat
@@ -4,6 +4,10 @@ cd /D %~dp0
 
 call BaseStorm.bat
 
+if not exist "%STORM_REPO_ROOT%\Config\Scenes\Original" (
+	mkdir "%STORM_REPO_ROOT%\Config\Scenes\Original"
+)
+
 echo We would overwrite the scenes configuration by the default templated one.
 
 copy "%STORM_REPO_ROOT%\Config\Template" "%STORM_REPO_ROOT%\Config\Scenes\Original"
