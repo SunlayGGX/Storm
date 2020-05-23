f87dd20c8cf66581b4442971876acf75b3cf7ad7
SunlayGGX <sunlay.g@free.fr>
2020-05-23 09:08:46 -0400

    Made a helper for Zeroing memory.

---
+++ Source/Storm-Helper/include/MemoryHelper.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Storm
+{
+    template<class Type, class ... Args>
+    void ZeroMemories(Type &val, Args &... others)
+    {
+        ZeroMemories(val);
+        ZeroMemories(others...);
+    }
+
+    template<class Type>
+    void ZeroMemories(Type &val)
+    {
+        ZeroMemory(&val, sizeof(Type));
+    }
+}
