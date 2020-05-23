dd1be3a098296fb7bfcc908ca70f14792a97b806
SunlayGGX <sunlay.g@free.fr>
2020-05-23 19:11:31 -0400

    Made an helper to transform a duration into a FPS value.

--- Source/Storm-Helper/include/InvertPeriod.h
+++ Source/Storm-Helper/include/InvertPeriod.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "NonInstanciable.h"
+
 
 namespace Storm
 {
@@ -10,5 +12,16 @@ namespace Storm
 	struct InvertPeriod<std::ratio<num, denum>>
 	{
 		static constexpr float value = static_cast<float>(denum) / static_cast<float>(num);
+	};
+
+	struct ChronoHelper : private Storm::NonInstanciable
+	{
+	public:
+		template<std::size_t bufferAverizeSize = 1, class DurationType>
+		static float toFps(const DurationType &duration)
+		{
+			constexpr float coefficientDurationToFps = Storm::InvertPeriod<DurationType::period>::value * static_cast<float>(bufferAverizeSize);
+			return coefficientDurationToFps / static_cast<float>(duration.count());
+		}
 	};
 }
