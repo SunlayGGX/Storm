c20f166b51372a28654eeb06cb1dfaafdf4d7b86
SunlayGGX <sunlay.g@free.fr>
2020-05-24 19:21:44 -0400

    Add a scene file reader. It isn't implemented yet.

---
+++ Source/Storm-Config/include/SceneFileReader.cpp
@@ -0,0 +1,20 @@
+#include "SceneFileReader.h"
+
+
+
+
+bool Storm::SceneFileReader::read(const std::string &sceneConfigFilePathStr)
+{
+	const std::filesystem::path sceneConfigFilePath{ sceneConfigFilePathStr };
+	if (std::filesystem::is_regular_file(sceneConfigFilePath))
+	{
+		if (sceneConfigFilePath.extension() == ".xml")
+		{
+			// TODO
+			throw std::runtime_error{ "non implemented yet" };
+			return true;
+		}
+	}
+
+	return false;
+}
---
+++ Source/Storm-Config/include/SceneFileReader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+namespace Storm
+{
+	class SceneFileReader
+	{
+	public:
+		bool read(const std::string &sceneConfigFilePathStr);
+	};
+}
