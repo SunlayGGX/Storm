bfd38850976517add21271749ea847d42b9fd4e6
SunlayGGX <sunlay.g@free.fr>
2020-05-24 19:04:16 -0400

    Made a new hierarchy for config files.

--- Build/Script/Internal/ScenesCopy.bat
+++ Build/Script/Internal/ScenesCopy.bat
@@ -4,10 +4,22 @@ cd /D %~dp0
 
 call BaseStorm.bat
 
-if not exist "%STORM_REPO_ROOT%\Config\Scenes\Original" (
-	mkdir "%STORM_REPO_ROOT%\Config\Scenes\Original"
-)
-
 echo We would overwrite the scenes configuration by the default templated one.
 
-copy "%STORM_REPO_ROOT%\Config\Template" "%STORM_REPO_ROOT%\Config\Scenes\Original"
+set copyCmd=call :makeCopyCmd
+
+%copyCmd% "%STORM_REPO_ROOT%\Config\Template\Scenes" "%STORM_REPO_ROOT%\Config\Custom\Scenes\Original"
+%copyCmd% "%STORM_REPO_ROOT%\Config\Template\General" "%STORM_REPO_ROOT%\Config\Custom\General\Original"
+
+
+exit /B %errorlevel%
+
+
+:makeCopyCmd
+if not errorlevel 1 (
+	if not exist "%~2" (
+		echo Creating folder "%~2"
+		mkdir "%~2"
+	)
+	copy "%~1" "%~2"
+)
