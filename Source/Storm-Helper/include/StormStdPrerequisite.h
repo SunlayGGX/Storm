194b7eb186a157ebdb07cc60a1031d88b262f651
SunlayGGX <sunlay.g@free.fr>
2020-05-23 20:08:34 -0400

    Mute a really annoying warning.

--- Source/Storm-Helper/include/StormStdPrerequisite.h
+++ Source/Storm-Helper/include/StormStdPrerequisite.h
@@ -18,3 +18,8 @@
 #include <atomic>
 #include <mutex>
 #include <thread>
+
+
+// 4624: 'XXXXXX': destructor was implicitly defined as deleted => Yes it is intended for classes that shouldn't be instantiated (abstract classes). NonInstanciable would always fire this warning so we need to disable it.
+#define STORM_DISABLE_WARNING_ALL 4624
+#pragma warning(disable: STORM_DISABLE_WARNING_ALL)
