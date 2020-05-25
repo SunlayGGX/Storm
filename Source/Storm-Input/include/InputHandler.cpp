d206813e6a4073c98713abe0ff8397c115760796
SunlayGGX <sunlay.g@free.fr>
2020-05-25 17:10:10 -0400

    Made an InputHandler from what I saw inside OIS demo.

---
+++ Source/Storm-Input/include/InputHandler.cpp
@@ -0,0 +1,131 @@
+#include "InputHandler.h"
+
+#include "SingletonHolder.h"
+#include "ISimulatorManager.h"
+#include "ITimeManager.h"
+
+
+namespace
+{
+	template<class FuncCallback>
+	auto makeForwardToSimulationLoopCallback(FuncCallback &&callback)
+	{
+		return [srcCallback = std::move(callback)]()
+		{
+			Storm::SingletonHolder::instance().getFacet<Storm::ISimulatorManager>()->executeOnSimulationLoop(srcCallback);
+		};
+	}
+}
+
+
+bool Storm::InputHandler::keyPressed(const OIS::KeyEvent &arg)
+{
+	if (arg.key != OIS::KeyCode::KC_ESCAPE)
+	{
+		std::lock_guard<std::mutex> lock{ _bindingMutex };
+		_keyBindings[arg.text]._onKeyPressed();
+	}
+	else
+	{
+		LOG_COMMENT << "Escape key was pressed, we will quit the application!";
+		Storm::SingletonHolder::instance().getFacet<Storm::ITimeManager>()->quit();
+	}
+
+	return true;
+}
+
+bool Storm::InputHandler::keyReleased(const OIS::KeyEvent &)
+{
+	// Unhandled
+	return true;
+}
+
+bool Storm::InputHandler::mouseMoved(const OIS::MouseEvent &)
+{
+	// Unhandled
+	return true;
+}
+
+bool Storm::InputHandler::mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+
+	switch (id)
+	{
+	case OIS::MB_Left:
+		_leftMouseButton._onClick();
+		break;
+
+	case OIS::MB_Right:
+		_rightMouseButton._onClick();
+		break;
+
+	case OIS::MB_Middle:
+		_middleMouseButton._onClick();
+		break;
+
+	case OIS::MB_Button3:
+	case OIS::MB_Button4:
+	case OIS::MB_Button5:
+	case OIS::MB_Button6:
+	case OIS::MB_Button7:
+	default:
+		break;
+	}
+
+	return true;
+}
+
+bool Storm::InputHandler::mouseReleased(const OIS::MouseEvent &, OIS::MouseButtonID)
+{
+	// Unhandled
+	return true;
+}
+
+Storm::CallbackIdType Storm::InputHandler::bindKey(unsigned int key, Storm::KeyBinding &&binding)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	return _keyBindings[key]._onKeyPressed.add(makeForwardToSimulationLoopCallback(std::move(binding)));
+}
+
+void Storm::InputHandler::unbindKey(unsigned int key, Storm::CallbackIdType callbackId)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	_keyBindings[key]._onKeyPressed.remove(callbackId);
+}
+
+Storm::CallbackIdType Storm::InputHandler::bindMouseRightClick(Storm::KeyBinding &&binding)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	return _rightMouseButton._onClick.add(makeForwardToSimulationLoopCallback(std::move(binding)));
+}
+
+void Storm::InputHandler::unbindMouseRightClick(Storm::CallbackIdType callbackId)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	_rightMouseButton._onClick.remove(callbackId);
+}
+
+Storm::CallbackIdType Storm::InputHandler::bindMouseLeftClick(Storm::KeyBinding &&binding)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	return _leftMouseButton._onClick.add(makeForwardToSimulationLoopCallback(std::move(binding)));
+}
+
+void Storm::InputHandler::unbindMouseLeftClick(Storm::CallbackIdType callbackId)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	_leftMouseButton._onClick.remove(callbackId);
+}
+
+Storm::CallbackIdType Storm::InputHandler::bindMouseMiddleClick(Storm::KeyBinding &&binding)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	return _middleMouseButton._onClick.add(makeForwardToSimulationLoopCallback(std::move(binding)));
+}
+
+void Storm::InputHandler::unbindMouseMiddleClick(Storm::CallbackIdType callbackId)
+{
+	std::lock_guard<std::mutex> lock{ _bindingMutex };
+	_middleMouseButton._onClick.remove(callbackId);
+}
---
+++ Source/Storm-Input/include/InputHandler.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+
+#include "MultiCallback.h"
+#include "InputEvents.h"
+
+
+namespace Storm
+{
+	class InputHandler :
+		public OIS::KeyListener,
+		public OIS::MouseListener
+	{
+	private:
+		enum
+		{
+			k_keyboardKeyBindingCount = 256
+		};
+
+	private:
+		struct KeyBindings
+		{
+			Storm::MultiCallback<Storm::KeyBinding> _onKeyPressed;
+		};
+
+		struct MouseBindings
+		{
+			Storm::MultiCallback<Storm::MouseBinding> _onClick;
+		};
+
+	public:
+		bool keyPressed(const OIS::KeyEvent &arg) final override;
+		bool keyReleased(const OIS::KeyEvent &arg) final override;
+		bool mouseMoved(const OIS::MouseEvent &arg) final override;
+		bool mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id) final override;
+		bool mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id) final override;
+
+public:
+		Storm::CallbackIdType bindKey(unsigned int key, Storm::KeyBinding &&binding);
+		void unbindKey(unsigned int key, Storm::CallbackIdType callbackId);
+		Storm::CallbackIdType bindMouseRightClick(Storm::KeyBinding &&binding);
+		void unbindMouseRightClick(Storm::CallbackIdType callbackId);
+		Storm::CallbackIdType bindMouseLeftClick(Storm::KeyBinding &&binding);
+		void unbindMouseLeftClick(Storm::CallbackIdType callbackId);
+		Storm::CallbackIdType bindMouseMiddleClick(Storm::KeyBinding &&binding);
+		void unbindMouseMiddleClick(Storm::CallbackIdType callbackId);
+
+	private:
+		std::mutex _bindingMutex;
+
+		std::map<unsigned int, KeyBindings> _keyBindings;
+		MouseBindings _leftMouseButton;
+		MouseBindings _rightMouseButton;
+		MouseBindings _middleMouseButton;
+	};
+}
