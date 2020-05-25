7603f776174b1f8ecbee33a6680db426ce556dd1
SunlayGGX <sunlay.g@free.fr>
2020-05-25 11:48:20 -0400

    Implemented the GeneralConfig.

---
+++ Source/Storm-Config/include/GeneralConfig.cpp
@@ -0,0 +1,88 @@
+#include "GeneralConfig.h"
+
+#include "MacroConfig.h"
+
+#include "ThrowException.h"
+#include "XmlReader.h"
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/algorithm/string.hpp>
+
+
+namespace
+{
+	Storm::LogLevel logLevelFromString(std::string &&levelStr)
+	{
+		boost::to_lower(levelStr);
+
+#define STORM_RETURN_IF_LEVEL_IS(LogLevelEnum) if (levelStr == boost::to_lower_copy(std::string{ #LogLevelEnum })) return Storm::LogLevel::LogLevelEnum
+
+		STORM_RETURN_IF_LEVEL_IS(Debug);
+		STORM_RETURN_IF_LEVEL_IS(DebugError);
+		STORM_RETURN_IF_LEVEL_IS(Comment);
+		STORM_RETURN_IF_LEVEL_IS(Fatal);
+		STORM_RETURN_IF_LEVEL_IS(Warning);
+		STORM_RETURN_IF_LEVEL_IS(Error);
+
+#undef STORM_RETURN_IF_LEVEL_IS
+
+		Storm::throwException<std::exception>("Unknown Log level : " + levelStr);
+	}
+}
+
+
+Storm::GeneralConfig::GeneralConfig() :
+	_logLevel{ Storm::LogLevel::Debug },
+	_overrideLogs{ true },
+	_removeLogsOlderThanDays{ -1 }
+{
+
+}
+
+bool Storm::GeneralConfig::read(const std::string &generalConfigFilePathStr)
+{
+	const std::filesystem::path generalConfigFilePath{ generalConfigFilePathStr };
+	if (std::filesystem::is_regular_file(generalConfigFilePath))
+	{
+		if (generalConfigFilePath.extension() == ".xml")
+		{
+			boost::property_tree::ptree xmlTree;
+			boost::property_tree::read_xml(generalConfigFilePathStr, xmlTree, boost::property_tree::xml_parser::no_comments);
+
+			const auto &generalTree = xmlTree.get_child("General");
+
+			/* Logs */
+			const auto &logTreeOpt = generalTree.get_child_optional("Log");
+			if (logTreeOpt.has_value())
+			{
+				const auto &logTree = logTreeOpt.value();
+				for (const auto &logXmlElement : logTree)
+				{
+					if (
+						!Storm::XmlReader::handleXml(logXmlElement, "logFolderPath", _logFolderPath) &&
+						!Storm::XmlReader::handleXml(logXmlElement, "logFileName", _logFileName) &&
+						!Storm::XmlReader::handleXml(logXmlElement, "logLevel", _logLevel, logLevelFromString) &&
+						!Storm::XmlReader::handleXml(logXmlElement, "override", _overrideLogs) &&
+						!Storm::XmlReader::handleXml(logXmlElement, "removeOlderThanDays", _removeLogsOlderThanDays)
+						)
+					{
+						LOG_ERROR << logXmlElement.first << " (inside General.Log) is unknown, therefore it cannot be handled";
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void Storm::GeneralConfig::applyMacros(const Storm::MacroConfig &macroConf)
+{
+	macroConf(_logFolderPath);
+	macroConf(_logFileName);
+
+	_logFileName = std::filesystem::path{ _logFileName }.filename().string();
+}
--- Source/Storm-Config/include/GeneralConfig.h
+++ Source/Storm-Config/include/GeneralConfig.h
@@ -3,8 +3,24 @@
 
 namespace Storm
 {
+	class MacroConfig;
+
 	class GeneralConfig
 	{
+	public:
+		GeneralConfig();
 
+	public:
+		bool read(const std::string &generalConfigFilePathStr);
+
+		void applyMacros(const Storm::MacroConfig &macroConf);
+
+	public:
+		// Logger
+		std::string _logFolderPath;
+		std::string _logFileName;
+		Storm::LogLevel _logLevel;
+		bool _overrideLogs;
+		int _removeLogsOlderThanDays;
 	};
 }
