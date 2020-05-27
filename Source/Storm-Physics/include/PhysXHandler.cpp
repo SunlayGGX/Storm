4daadc3aafb736d6d116fc427848fc1729ba459e
SunlayGGX <sunlay.g@free.fr>
2020-05-27 08:53:53 -0400

    Logs before throwing.

--- Source/Storm-Physics/include/PhysXHandler.cpp
+++ Source/Storm-Physics/include/PhysXHandler.cpp
@@ -15,15 +15,15 @@ namespace
 Storm::PhysXHandler::PhysXHandler() :
 	_foundationInstance{ PxCreateFoundation(PX_PHYSICS_VERSION, g_defaultAllocator, g_physXLogger) }
 {
-	if (_foundationInstance != nullptr)
-	{
-		Storm::throwException<std::exception>("PhysX foundation couldn't be created!");
-	}
-
 	constexpr Storm::Version currentPhysXVersion{ PX_PHYSICS_VERSION_MAJOR, PX_PHYSICS_VERSION_MINOR, PX_PHYSICS_VERSION_BUGFIX };
 	if (currentPhysXVersion != Storm::Version{ 4, 0, 0 })
 	{
 		LOG_WARNING << "PhysX version mismatches with what we used in our development. What we use currently is PhysX version " << currentPhysXVersion << " and what we used was PhysX version 4.0.0...";
+	}
+
+	if (_foundationInstance != nullptr)
+	{
+		Storm::throwException<std::exception>("PhysX foundation couldn't be created!");
 	}
 }
 
