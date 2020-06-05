30331546a30fbb08a6952955887e1dcf94ac54f5
SunlayGGX <sunlay.g@free.fr>
2020-06-05 01:36:09 -0400

    Prepare the PhysX engine.

--- Source/Storm-Physics/include/PhysXHandler.cpp
+++ Source/Storm-Physics/include/PhysXHandler.cpp
@@ -5,6 +5,11 @@
 #include "ThrowException.h"
 #include "Version.h"
 
+#include "SingletonHolder.h"
+#include "IConfigManager.h"
+
+#include "GeneralSimulationData.h"
+
 
 namespace
 {
@@ -25,9 +30,72 @@ Storm::PhysXHandler::PhysXHandler() :
 	{
 		Storm::throwException<std::exception>("PhysX foundation couldn't be created!");
 	}
+
+
+	// Create the Physics object.
+
+#if defined(_DEBUG) || defined(DEBUG)
+	constexpr bool recordMemoryAlloc = true;
+#else
+	constexpr bool recordMemoryAlloc = false;
+#endif
+
+	physx::PxTolerancesScale toleranceScale;
+	_physics = PxCreatePhysics(PX_PHYSICS_VERSION, *_foundationInstance, toleranceScale, recordMemoryAlloc);
+	if (!_physics)
+	{
+		Storm::throwException<std::exception>("PxCreatePhysics failed! We cannot use PhysX features, aborting!");
+	}
+
+	_physics->registerDeletionListener(*this, physx::PxDeletionEventFlag::Enum::eUSER_RELEASE);
+
+
+	// Create the PhysX Scene
+
+	Storm::SingletonHolder &singletonHolder = Storm::SingletonHolder::instance();
+	const Storm::IConfigManager &configMgr = singletonHolder.getSingleton<Storm::IConfigManager>();
+
+	const Storm::GeneralSimulationData &simulData = configMgr.getGeneralSimulationData();
+
+	physx::PxSceneDesc sceneDesc{ _physics->getTolerancesScale() };
+	sceneDesc.gravity = physx::PxVec3{ simulData._gravity.x(), simulData._gravity.y(), simulData._gravity.z() };
+
+	sceneDesc.flags |= physx::PxSceneFlag::eENABLE_PCM;
+	sceneDesc.gpuMaxNumPartitions = 8;
+
+	_scene = _physics->createScene(sceneDesc);
+	if (!_scene)
+	{
+		Storm::throwException<std::exception>("PhysX main scene creation failed!");
+	}
 }
 
 Storm::PhysXHandler::~PhysXHandler()
 {
+	_physics->unregisterDeletionListener(*this);
+}
 
+physx::PxPhysics& Storm::PhysXHandler::getPhysics() const
+{
+	return *_physics;
+}
+
+void Storm::PhysXHandler::onRelease(const physx::PxBase* observed, void* userData, physx::PxDeletionEventFlag::Enum deletionEvent)
+{
+	PX_UNUSED(userData);
+	PX_UNUSED(deletionEvent);
+
+	if (observed->is<physx::PxRigidActor>())
+	{
+		LOG_DEBUG << "A PhysX actor was just released!";
+		// TODO : Do more than this...
+	}
+}
+
+void Storm::PhysXHandler::setGravity(const Storm::Vector3 &newGravity)
+{
+	physx::PxVec3 physXGravity{ newGravity.x(), newGravity.y(), newGravity.z() };
+
+	physx::PxSceneWriteLock lock{ *_scene };
+	_scene->setGravity(physXGravity);
 }
--- Source/Storm-Physics/include/PhysXHandler.h
+++ Source/Storm-Physics/include/PhysXHandler.h
@@ -5,13 +5,24 @@
 
 namespace Storm
 {
-	class PhysXHandler
+	class PhysXHandler : public physx::PxDeletionListener
 	{
 	public:
 		PhysXHandler();
 		~PhysXHandler();
 
+	public:
+		physx::PxPhysics& getPhysics() const;
+
+	public:
+		void onRelease(const physx::PxBase* observed, void* userData, physx::PxDeletionEventFlag::Enum deletionEvent) final override;
+
+	public:
+		void setGravity(const Storm::Vector3 &newGravity);
+
 	private:
 		Storm::UniquePointer<physx::PxFoundation> _foundationInstance;
+		Storm::UniquePointer<physx::PxPhysics> _physics;
+		Storm::UniquePointer<physx::PxScene> _scene;
 	};
 }
