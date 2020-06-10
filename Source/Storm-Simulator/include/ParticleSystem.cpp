aa8981be55058a6f825195e007f7d7f4ae8bac42
SunlayGGX <sunlay.g@free.fr>
2020-06-10 09:23:00 -0400

    Add some basics particles data arrays.

--- Source/Storm-Simulator/include/ParticleSystem.cpp
+++ Source/Storm-Simulator/include/ParticleSystem.cpp
@@ -1,14 +1,37 @@
 #include "ParticleSystem.h"
 
 
+namespace
+{
+	float computeDefaultParticleMass()
+	{
+		// TODO
+		return 1.f;
+	}
+}
+
 
 Storm::ParticleSystem::ParticleSystem(std::vector<Storm::Vector3> &&worldPositions) :
 	_positions{ std::move(worldPositions) }
 {
+	const std::size_t particleCount = _positions.size();
 
+	_masses.resize(particleCount, computeDefaultParticleMass());
+	_velocity.resize(particleCount, Storm::Vector3::Zero());
+	_accelerations.resize(particleCount);
 }
 
-const std::vector<Storm::Vector3>& Storm::ParticleSystem::getPositions() const
+const std::vector<Storm::Vector3>& Storm::ParticleSystem::getPositions() const noexcept
 {
 	return _positions;
+}
+
+const std::vector<Storm::Vector3>& Storm::ParticleSystem::getVelocity() const noexcept
+{
+	return _velocity;
+}
+
+const std::vector<Storm::Vector3>& Storm::ParticleSystem::getAccelerations() const noexcept
+{
+	return _accelerations;
 }
--- Source/Storm-Simulator/include/ParticleSystem.h
+++ Source/Storm-Simulator/include/ParticleSystem.h
@@ -10,11 +10,16 @@ namespace Storm
 		virtual ~ParticleSystem() = default;
 
 	public:
-		const std::vector<Storm::Vector3>& getPositions() const;
+		const std::vector<Storm::Vector3>& getPositions() const noexcept;
+		const std::vector<Storm::Vector3>& getVelocity() const noexcept;
+		const std::vector<Storm::Vector3>& getAccelerations() const noexcept;
 
 		virtual bool isFluids() const noexcept = 0;
 
 	protected:
+		std::vector<float> _masses;
 		std::vector<Storm::Vector3> _positions;
+		std::vector<Storm::Vector3> _velocity;
+		std::vector<Storm::Vector3> _accelerations;
 	};
 }
