10994f4fcb64b5aa912ead3ff93fc4d96f936839
SunlayGGX <sunlay.g@free.fr>
2020-08-14 20:20:32 -0400

    Add some method to allow the SpaceManager to compile. But those methods aren't implemented yet.

--- Source/Storm-Space/include/VoxelGrid.cpp
+++ Source/Storm-Space/include/VoxelGrid.cpp
@@ -1,2 +1,25 @@
 #include "VoxelGrid.h"
 
+#include "StormMacro.h"
+#include "ThrowException.h"
+
+
+Storm::VoxelGrid::VoxelGrid(const Storm::Vector3 &upCorner, const Storm::Vector3 &downCorner, float voxelEdgeLength)
+{
+	STORM_NOT_IMPLEMENTED;
+}
+
+void Storm::VoxelGrid::getVoxelsDataAtPosition(const std::vector<std::size_t>* &outContainingVoxelPtr, std::vector<const std::vector<std::size_t>*> &outNeighborData, const Storm::Vector3 &particlePosition) const
+{
+	STORM_NOT_IMPLEMENTED;
+}
+
+void Storm::VoxelGrid::fill(const std::vector<Storm::Vector3> &particlePositions)
+{
+	STORM_NOT_IMPLEMENTED;
+}
+
+void Storm::VoxelGrid::clear()
+{
+	STORM_NOT_IMPLEMENTED;
+}
--- Source/Storm-Space/include/VoxelGrid.h
+++ Source/Storm-Space/include/VoxelGrid.h
@@ -5,6 +5,16 @@ namespace Storm
 {
 	class VoxelGrid
 	{
+	public:
+		VoxelGrid(const Storm::Vector3 &upCorner, const Storm::Vector3 &downCorner, float voxelEdgeLength);
 
+	public:
+		void getVoxelsDataAtPosition(const std::vector<std::size_t>* &outContainingVoxelPtr, std::vector<const std::vector<std::size_t>*> &outNeighborData, const Storm::Vector3 &particlePosition) const;
+
+		void fill(const std::vector<Storm::Vector3> &particlePositions);
+		void clear();
+
+	private:
+		
 	};
 }
