e63b56a6765162a9f14949aa00c4d37b77696229
SunlayGGX <sunlay.g@free.fr>
2020-08-20 11:54:23 -0400

    Add support of a large variety of object for throwException.

--- Source/Storm-Helper/include/ThrowException.h
+++ Source/Storm-Helper/include/ThrowException.h
@@ -1,32 +1,34 @@
 #pragma once
 
+#include "UniversalString.h"
+
 
 namespace Storm
 {
 	namespace details
 	{
 		template<class ExceptionType, class ... ArgType>
-		[[noreturn]] auto throwExceptionImpl(int, ArgType &&... arg) -> decltype(ExceptionType{ std::forward<ArgType>(arg)... }, void())
+		[[noreturn]] auto throwExceptionImpl(int, int, ArgType &&... arg) -> decltype(ExceptionType{ std::forward<ArgType>(arg)... }, void())
 		{
 			throw ExceptionType{ std::forward<ArgType>(arg)... };
 		}
 
 		template<class ExceptionType, class StringType>
-		[[noreturn]] auto throwExceptionImpl(void*, const StringType &msg) -> decltype(ExceptionType{ msg.c_str() }, void())
+		[[noreturn]] auto throwExceptionImpl(void*, int, const StringType &msg) -> decltype(ExceptionType{ msg.c_str() }, void())
 		{
 			throw ExceptionType{ msg.c_str() };
 		}
 
 		template<class ExceptionType, class StringType>
-		[[noreturn]] auto throwExceptionImpl(void*, const StringType &msg) -> decltype(Storm::details::throwExceptionImpl<ExceptionType>(0, msg.str()), void())
+		[[noreturn]] auto throwExceptionImpl(void*, void*, const StringType &msg) -> decltype(Storm::details::throwExceptionImpl<ExceptionType>(0, 0, Storm::toStdString(msg)), void())
 		{
-			Storm::details::throwExceptionImpl<ExceptionType>(0, msg.str());
+			Storm::details::throwExceptionImpl<ExceptionType>(0, 0, Storm::toStdString(msg));
 		}
 	}
 
 	template<class ExceptionType, class ... ArgType>
 	[[noreturn]] void throwException(ArgType &&... arg)
 	{
-		Storm::details::throwExceptionImpl<ExceptionType>(0, std::forward<ArgType>(arg)...);
+		Storm::details::throwExceptionImpl<ExceptionType>(0, 0, std::forward<ArgType>(arg)...);
 	}
 }
