50cb9193420b959116b745121b447e785f80f98a
SunlayGGX <sunlay.g@free.fr>
2020-08-20 12:11:04 -0400

    Add throwIfExist tests.

--- Source/Automation/StormAutomation-HelperTester/include/SearchAlgoTester.cpp
+++ Source/Automation/StormAutomation-HelperTester/include/SearchAlgoTester.cpp
@@ -9,6 +9,44 @@ namespace
 		bool operator<(const std::string &val) const { return _val < val; }
 		std::string _val;
 	};
+
+	template<bool shouldThrow, class ContainerType, class ValueOrPredType>
+	void testThrowIfExist(const ContainerType &cont, const ValueOrPredType &valueOrPred)
+	{
+		constexpr std::string_view existValue = "It exists!";
+
+		try
+		{
+			Storm::SearchAlgo::throwIfExist(cont, existValue, valueOrPred);
+
+			if constexpr (shouldThrow)
+			{
+				FAIL("Storm::SearchAlgo::throwIfExist should have thrown, but it didn't!");
+			}
+		}
+		catch (const std::exception &ex)
+		{
+			if constexpr (shouldThrow)
+			{
+				CHECK(existValue == ex.what());
+			}
+			else
+			{
+				FAIL("Storm::SearchAlgo::throwIfExist have thrown, but it shouldn't have!");
+			}
+		}
+		catch (...)
+		{
+			if constexpr (shouldThrow)
+			{
+				FAIL("Storm::SearchAlgo::throwIfExist haven't thrown a std exception!");
+			}
+			else
+			{
+				FAIL("Storm::SearchAlgo::throwIfExist have thrown, but it shouldn't have!");
+			}
+		}
+	}
 }
 
 TEST_CASE("SearchAlgo.searchIfExist.vector", "[classic]")
@@ -41,3 +79,32 @@ TEST_CASE("SearchAlgo.searchIfExist.map", "[classic]")
 	CHECK(Storm::SearchAlgo::searchIfExist(mapObj, [toCheck = std::string{ "tata" }](const std::pair<int, Obj> &val) { return val.second == toCheck; }) == false);
 }
 
+TEST_CASE("SearchAlgo.throwIfExist.vector", "[classic]")
+{
+	const std::vector<std::string> strVect{ "toto", "titi", "tutu" };
+	const std::vector<Obj> objVect{ Obj{ "toto" }, Obj{ "titi" }, Obj{ "tutu" } };
+
+	testThrowIfExist<true>(strVect, "toto");
+	testThrowIfExist<false>(strVect, "tata");
+
+	testThrowIfExist<true>(objVect, "toto");
+	testThrowIfExist<false>(objVect, "tata");
+
+	testThrowIfExist<true>(objVect, [toCheck = std::string{ "toto" }](const Obj &val) { return val == toCheck; });
+	testThrowIfExist<false>(objVect, [toCheck = std::string{ "tata" }](const Obj &val) { return val == toCheck; });
+}
+
+TEST_CASE("SearchAlgo.throwIfExist.map", "[classic]")
+{
+	const std::map<int, std::string> mapStr{ std::pair<int, std::string>{ 0, "toto" }, std::pair<int, std::string>{ 1, "titi" }, std::pair<int, std::string>{ -5, "tutu" } };
+	const std::map<int, Obj> mapObj{ std::pair<int, Obj>{ 0, Obj{ "toto" } }, std::pair<int, Obj>{ 1, Obj{ "titi" } }, std::pair<int, Obj>{ -5, Obj{ "tutu" } } };
+
+	testThrowIfExist<true>(mapStr, "toto");
+	testThrowIfExist<false>(mapStr, "tata");
+
+	testThrowIfExist<true>(mapObj, "toto");
+	testThrowIfExist<false>(mapObj, "tata");
+
+	testThrowIfExist<true>(mapObj, [toCheck = std::string{ "toto" }](const std::pair<int, Obj> &val) { return val.second == toCheck; });
+	testThrowIfExist<false>(mapObj, [toCheck = std::string{ "tata" }](const std::pair<int, Obj> &val) { return val.second == toCheck; });
+}
