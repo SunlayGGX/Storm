e3adda7e963f4397edcfcbb3f1e41d22bd023e7e
SunlayGGX <sunlay.g@free.fr>
2020-08-19 21:33:09 -0400

    Disable the useless waring of the destructor implicitly deleted crap.

--- Source/Automation/StormAutomation-Base/include/StormAutomationPrerequisite.h
+++ Source/Automation/StormAutomation-Base/include/StormAutomationPrerequisite.h
@@ -4,3 +4,7 @@
 #include "StormStdPrerequisite.h"
 
 #include <catch2/catch.hpp>
+
+// 4624 : 'XXXX': destructor was implicitly defined as deleted => I did it on purpose!... It is to make non instanciable class (purely abstract class).
+#define STORM_DISABLED_WARNINGS 4624
+#pragma warning(disable: STORM_DISABLED_WARNINGS)
