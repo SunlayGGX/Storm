962360c540b402544b7cbe56cdb46b364c259b79
SunlayGGX <sunlay.g@free.fr>
2020-08-21 13:08:31 -0400

    Fix SizeCounter tests not passing in release because of std::string size different between release and debug.

--- Source/Automation/StormAutomation-HelperTester/include/MetaprogTester.cpp
+++ Source/Automation/StormAutomation-HelperTester/include/MetaprogTester.cpp
@@ -1,10 +1,21 @@
 #include "SizeCounter.h"
 
+namespace
+{
+	class Object
+	{
+	private:
+		int32_t _1;
+		double _2;
+		double* _3;
+		uint64_t _4;
+	};
+}
 
 TEST_CASE("SizeCounter", "[classic]")
 {
 	CHECK(Storm::SizeCounter<double>::value == 8);
-	CHECK(Storm::SizeCounter<double, double, double, float, std::string>::value == 68);
-	CHECK(Storm::SizeCounter<double, double, float, std::string>::value == 60);
+	CHECK(Storm::SizeCounter<double, double, double, float, Object>::value == 60);
+	CHECK(Storm::SizeCounter<double, double, float, Object>::value == 52);
 	CHECK(Storm::SizeCounter<int32_t[24]>::value == 96);
 }
