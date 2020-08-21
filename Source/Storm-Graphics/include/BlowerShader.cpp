af5df5b3496f2a4d3d278af24d83802be8ce24ae
SunlayGGX <sunlay.g@free.fr>
2020-08-21 13:49:30 -0400

    Base implementation of the BlowerShader constructor that construct and compile the associated shader + minimal implementation of the said shader.

--- Source/Storm-Graphics/include/BlowerShader.cpp
+++ Source/Storm-Graphics/include/BlowerShader.cpp
@@ -1,7 +1,67 @@
 #include "BlowerShader.h"
 
+#include "MemoryHelper.h"
 
-Storm::BlowerShader::BlowerShader(const ComPtr<ID3D11Device> &device, const uint32_t indexCount, const DirectX::XMMATRIX &blowerWorldMatrix)
+#include "Camera.h"
+
+namespace
 {
+	struct ConstantBuffer
+	{
+		DirectX::XMMATRIX _worldMatrix;
+		DirectX::XMMATRIX _viewMatrix;
+		DirectX::XMMATRIX _projectionMatrix;
+	};
 
+	static const std::string k_blowerShaderFilePath = "Shaders/BlowerDraw.hlsl";
+	static constexpr std::string_view k_blowerVertexShaderFuncName = "blowerVertexShader";
+	static constexpr std::string_view k_blowerPixelShaderFuncName = "blowerPixelShader";
+
+	enum : unsigned int
+	{
+		k_blowerVertexDataLayoutDescCount = 1
+	};
+
+	inline D3D11_INPUT_ELEMENT_DESC* retrieveBlowerInputLayoutElementDesc()
+	{
+		static D3D11_INPUT_ELEMENT_DESC meshVertexDataLayoutDesc[k_blowerVertexDataLayoutDescCount];
+
+		D3D11_INPUT_ELEMENT_DESC &currentVertexDataLayoutDesc = meshVertexDataLayoutDesc[0];
+		currentVertexDataLayoutDesc.SemanticName = "POSITION";
+		currentVertexDataLayoutDesc.SemanticIndex = 0;
+		currentVertexDataLayoutDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R32G32B32A32_FLOAT;
+		currentVertexDataLayoutDesc.InputSlot = 0;
+		currentVertexDataLayoutDesc.AlignedByteOffset = 0;
+		currentVertexDataLayoutDesc.InputSlotClass = D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA;
+		currentVertexDataLayoutDesc.InstanceDataStepRate = 0;
+
+		return meshVertexDataLayoutDesc;
+	}
+}
+
+Storm::BlowerShader::BlowerShader(const ComPtr<ID3D11Device> &device, const uint32_t indexCount) :
+	Storm::VPShaderBase{ device, k_blowerShaderFilePath, k_blowerVertexShaderFuncName, k_blowerShaderFilePath, k_blowerPixelShaderFuncName, retrieveBlowerInputLayoutElementDesc(), k_blowerVertexDataLayoutDescCount }
+{
+	Storm::ConstantBufferHolder::initialize<ConstantBuffer>(device);
+}
+
+void Storm::BlowerShader::setup(const ComPtr<ID3D11Device> &device, const ComPtr<ID3D11DeviceContext> &deviceContext, const Storm::Camera &currentCamera, const DirectX::XMMATRIX &transposedTransform)
+{
+	this->setupDeviceContext(deviceContext);
+
+	// Write shaders parameters
+	D3D11_MAPPED_SUBRESOURCE blowerConstantBufferRessource;
+	Storm::throwIfFailed(deviceContext->Map(_constantBuffer.Get(), 0, D3D11_MAP::D3D11_MAP_WRITE_DISCARD, 0, &blowerConstantBufferRessource));
+
+	ConstantBuffer*const ressourceDataPtr = static_cast<ConstantBuffer*>(blowerConstantBufferRessource.pData);
+
+	ressourceDataPtr->_worldMatrix = transposedTransform;
+	ressourceDataPtr->_viewMatrix = currentCamera.getTransposedViewMatrix();
+	ressourceDataPtr->_projectionMatrix = currentCamera.getTransposedProjectionMatrix();
+
+	deviceContext->Unmap(_constantBuffer.Get(), 0);
+
+	ID3D11Buffer*const constantBufferTmp = _constantBuffer.Get();
+	deviceContext->VSSetConstantBuffers(0, 1, &constantBufferTmp);
+	deviceContext->PSSetConstantBuffers(0, 1, &constantBufferTmp);
 }
--- Source/Storm-Graphics/include/BlowerShader.h
+++ Source/Storm-Graphics/include/BlowerShader.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "VPShader.h"
+#include "ConstantBufferHolder.h"
+
 
 namespace Storm
 {
-	class BlowerShader
+	class BlowerShader :
+		public Storm::VPShaderBase,
+		private Storm::ConstantBufferHolder
 	{
 	public:
 		BlowerShader(const ComPtr<ID3D11Device> &device, const uint32_t indexCount, const DirectX::XMMATRIX &blowerWorldMatrix);
