728413ba98a7fab6a9c65aa4a9818f55617e1985
SunlayGGX <sunlay.g@free.fr>
2020-09-01 22:17:47 -0400

    Add the Cylinder blower (declare it inside the X-Macro, therefore all needed code was generetated over a lot of places inside different modules.

--- Source/Storm-ModelBase/include/BlowerDef.h
+++ Source/Storm-ModelBase/include/BlowerDef.h
@@ -7,6 +7,7 @@
 #define STORM_XMACRO_GENERATE_BLOWERS_CODE																							\
 STORM_XMACRO_GENERATE_ELEMENTARY_BLOWER(Cube, "cube", BlowerCubeArea, BlowerCubeMeshMaker)											\
 STORM_XMACRO_GENERATE_ELEMENTARY_BLOWER(Sphere, "sphere", BlowerSphereArea, BlowerSphereMeshMaker)									\
+STORM_XMACRO_GENERATE_ELEMENTARY_BLOWER(Cylinder, "cylinder", BlowerCylinderArea, BlowerCylinderMeshMaker)							\
 STORM_XMACRO_GENERATE_ELEMENTARY_BLOWER(RepulsionSphere, "repulsionsphere", BlowerRepulsionSphereArea, BlowerSphereMeshMaker)		\
 STORM_XMACRO_GENERATE_ELEMENTARY_BLOWER(ExplosionSphere, "explosion", BlowerExplosionSphereArea, BlowerSphereMeshMaker)				\
 STORM_XMACRO_GENERATE_ELEMENTARY_BLOWER(PulseExplosionSphere, "pulseexplosion", BlowerExplosionSphereArea, BlowerSphereMeshMaker)
