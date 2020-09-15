3132bfe0600810fe08934ea310bd1fce77599e9d
SunlayGGX <sunlay.g@free.fr>
2020-09-15 14:29:43 -0400

    Add the interface for RaycastManager.

---
+++ Source/Storm-ModelBase/include/IRaycastManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "SingletonHeldInterfaceBase.h"
+
+
+namespace Storm
+{
+	enum class PartitionSelection;
+	struct NeighborParticleReferral;
+
+	using HitResponseCallback = std::function<void(std::vector<Storm::NeighborParticleReferral> &&)>;
+
+	class IRaycastManager : public Storm::ISingletonHeldInterface<Storm::IRaycastManager>
+	{
+	public:
+		virtual ~IRaycastManager() = default;
+
+	public:
+		virtual void queryRayCast(const Storm::Vector3 &origin, const Storm::Vector3 &direction, const std::vector<Storm::PartitionSelection> &hitFlag, HitResponseCallback callback) const = 0;
+		virtual void queryRayCast(const Storm::Vector2 &pixelScreenPos, const std::vector<Storm::PartitionSelection> &hitFlag, HitResponseCallback callback) const = 0;
+	};
+}
