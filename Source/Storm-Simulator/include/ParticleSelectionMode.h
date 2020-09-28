ebf0021b71d359e5fd9f0925261df0f3154fcd47
SunlayGGX <sunlay.g@free.fr>
2020-09-28 18:25:54 -0400

    Made an enumeration to select what to display in the selected particle.

--- Source/Storm-Simulator/include/ParticleSelectionMode.h
+++ Source/Storm-Simulator/include/ParticleSelectionMode.h
@@ -1,1 +1,17 @@
 #pragma once
+
+
+namespace Storm
+{
+	// What should we display.
+	// Do not set the value manually, let the compiler do its job.
+	enum class ParticleSelectionMode : uint8_t
+	{
+		Pressure,
+		Viscosity,
+		ViscosityAndPressure,
+
+		// Should remain the last
+		SelectionModeCount
+	};
+}
