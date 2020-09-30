827853526d465d57b99ccb923130a9ed9de22f73
SunlayGGX <sunlay.g@free.fr>
2020-09-30 10:58:32 -0400

    Prepare the algorithm of PCISPH (this is a dummy commit to push what I have stashed because I don't want to loose this stash among the stash quantity that starts to grow...). This commit does nothing more than preparing the algo by fetching some config setting values... So no change in what does nothing. I hope I would have time to advance the algorithm but I'm currently on another feature as important as this one (but more urgent, therefore I'll be doing the other feature before this one).

--- Source/Storm-Simulator/include/SPHSolvers.cpp
+++ Source/Storm-Simulator/include/SPHSolvers.cpp
@@ -195,4 +195,15 @@ void Storm::WCSPHSolver::execute(const std::map<unsigned int, std::unique_ptr<St
 void Storm::PCISPHSolver::execute(const std::map<unsigned int, std::unique_ptr<Storm::ParticleSystem>> &particleSystems, const float k_kernelLength, Storm::ParticleSelector &selectorParticle)
 {
 	STORM_NOT_IMPLEMENTED;
+
+
+	const Storm::IConfigManager &configMgr = Storm::SingletonHolder::instance().getSingleton<Storm::IConfigManager>();
+	const Storm::GeneralSimulationData &generalSimulData = configMgr.getGeneralSimulationData();
+	const Storm::FluidData &fluidConfigData = configMgr.getFluidData();
+
+	const float k_maxDensityError = generalSimulData._maxDensityError;
+	const unsigned int k_maxPredictionIter = generalSimulData._maxPredictIteration;
+	unsigned int currentPredictionIter = 0;
+
+
 }
