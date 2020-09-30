5c430823991283676f1fad499bc174a53d677ebf
SunlayGGX <sunlay.g@free.fr>
2020-09-29 23:10:39 -0400

    Made registered another thread (the Serializer module will have its own thread pending data to be serialized asynchronously).

--- Source/Storm-Serializer/include/SerializerManager.cpp
+++ Source/Storm-Serializer/include/SerializerManager.cpp
@@ -1,5 +1,44 @@
 #include "SerializerManager.h"
 
+#include "SingletonHolder.h"
+#include "IThreadManager.h"
+#include "ITimeManager.h"
+
+#include "ThreadEnumeration.h"
+
+#include "ThreadHelper.h"
+
 
 Storm::SerializerManager::SerializerManager() = default;
 Storm::SerializerManager::~SerializerManager() = default;
+
+void Storm::SerializerManager::initialize_Implementation()
+{
+	LOG_COMMENT << "Starting Serializer module initialization";
+
+	_serializeThread = std::thread{ [this]()
+	{
+		STORM_REGISTER_THREAD(SerializerThread);
+		this->run();
+	} };
+
+	LOG_COMMENT << "Serializer module initialization finished";
+}
+
+void Storm::SerializerManager::cleanUp_Implementation()
+{
+	LOG_COMMENT << "Serializer module Cleanup";
+	Storm::join(_serializeThread);
+}
+
+void Storm::SerializerManager::run()
+{
+	const Storm::SingletonHolder &singletonHolder = Storm::SingletonHolder::instance();
+	Storm::ITimeManager &timeMgr = singletonHolder.getSingleton<Storm::ITimeManager>();
+	Storm::IThreadManager &threadMgr = singletonHolder.getSingleton<Storm::IThreadManager>();
+
+	while (timeMgr.waitNextFrameOrExit())
+	{
+		threadMgr.processCurrentThreadActions();
+	}
+}
--- Source/Storm-Serializer/include/SerializerManager.h
+++ Source/Storm-Serializer/include/SerializerManager.h
@@ -2,15 +2,24 @@
 
 #include "Singleton.h"
 #include "ISerializerManager.h"
-#include "SingletonDefaultImplementation.h"
 
 
 namespace Storm
 {
 	class SerializerManager :
-		private Storm::Singleton<Storm::SerializerManager, Storm::DefineDefaultInitAndCleanupImplementation>,
+		private Storm::Singleton<Storm::SerializerManager>,
 		public Storm::ISerializerManager
 	{
 		STORM_DECLARE_SINGLETON(SerializerManager);
+
+	private:
+		void initialize_Implementation();
+		void cleanUp_Implementation();
+
+	private:
+		void run();
+
+	private:
+		std::thread _serializeThread;
 	};
 }
