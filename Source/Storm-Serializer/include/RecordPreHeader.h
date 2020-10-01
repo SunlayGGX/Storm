83ce17982e9fb796e3a8778162ecf3b94bb8d526
SunlayGGX <sunlay.g@free.fr>
2020-09-30 23:05:50 -0400

    Made a preheader class for recording file.

---
+++ Source/Storm-Serializer/include/RecordPreHeader.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Version.h"
+
+
+namespace Storm
+{
+	// Do not change this, otherwise you'll break the retrocompatibility.
+	// This is the minimal header part that tells us how to read the Header or what comes next.
+	struct RecordPreHeader
+	{
+	public:
+		uint32_t _magicWordChecksum; // Not a real checksum, just a magic word to tell us the record file was completed.
+		Storm::Version _recordVersion;
+		std::size_t _recordFileSize;
+	};
+}
