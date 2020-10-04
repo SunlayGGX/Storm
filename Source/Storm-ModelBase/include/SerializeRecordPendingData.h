90627c86c5486711a20721312a75246407c3046b
SunlayGGX <sunlay.g@free.fr>
2020-10-04 12:06:48 -0400

    Fill the SerializeRecordPendingData class.

--- Source/Storm-ModelBase/include/SerializeRecordPendingData.h
+++ Source/Storm-ModelBase/include/SerializeRecordPendingData.h
@@ -3,8 +3,18 @@
 
 namespace Storm
 {
+	struct SerializeRecordElementsData
+	{
+		uint32_t _systemId;
+		std::vector<Storm::Vector3> _positions;
+		std::vector<Storm::Vector3> _velocities;
+		std::vector<Storm::Vector3> _forces;
+	};
+
 	struct SerializeRecordPendingData
 	{
-
+	public:
+		float _physicsTime;
+		std::vector<Storm::SerializeRecordElementsData> _elements;
 	};
 }
