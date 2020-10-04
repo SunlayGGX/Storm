a03906358af1dd549ef27cb42dc4ad89e2b51bcd
SunlayGGX <sunlay.g@free.fr>
2020-10-04 11:54:22 -0400

    Moved the SerializeParticleSystemLayout internal class outside its encasing class to allow forward declaration.

--- Source/Storm-ModelBase/include/SerializeRecordHeader.h
+++ Source/Storm-ModelBase/include/SerializeRecordHeader.h
@@ -3,18 +3,17 @@
 
 namespace Storm
 {
+	struct SerializeParticleSystemLayout
+	{
+		uint32_t _particleSystemId;
+		uint64_t _particlesCount;
+		bool _isFluid;
+	};
+
 	struct SerializeRecordHeader
 	{
 	public:
-		struct ParticleSystemLayout
-		{
-			uint32_t _particleSystemId;
-			std::size_t _particlesCount;
-			bool _isFluid;
-		};
-
-	public:
 		float _recordFrameRate;
-		std::vector<Storm::SerializeRecordHeader::ParticleSystemLayout> _particleSystemLayouts;
+		std::vector<Storm::SerializeParticleSystemLayout> _particleSystemLayouts;
 	};
 }
