c298c5fff6fd9ed2f9016e45e70c6999d7598006
SunlayGGX <sunlay.g@free.fr>
2020-10-18 13:02:07 -0400

    Add a new vector type for double precision computation.

--- Source/Storm-ModelBase/include/Vector3.h
+++ Source/Storm-ModelBase/include/Vector3.h
@@ -9,6 +9,7 @@ namespace Storm
 	using Vector3ui = Eigen::Matrix<unsigned int, 3, 1>;
 	using Vector2 = Eigen::Vector2f;
 	using Vector3 = Eigen::Vector3f;
+	using Vector3d = Eigen::Vector3d;
 	using Vector4 = Eigen::Vector4f;
 	using Quaternion = Eigen::Quaternionf;
 }
