d5eeeb6b361109a3b949629d22f43961c8f6f610
SunlayGGX <sunlay.g@free.fr>
2020-10-19 10:37:57 -0400

    Removed a redondent ref constness.

--- Source/Storm-Helper/include/VectHijack.h
+++ Source/Storm-Helper/include/VectHijack.h
@@ -15,7 +15,7 @@ namespace Storm
 // Ugly but super efficient...
 template<>
 template<>
-inline decltype(auto) std::vector<STORM_HIJACKED_TYPE, std::allocator<STORM_HIJACKED_TYPE>>::emplace_back<const Storm::VectorHijackerMakeBelieve &>(const Storm::VectorHijackerMakeBelieve &hijacker)
+inline decltype(auto) std::vector<STORM_HIJACKED_TYPE, std::allocator<STORM_HIJACKED_TYPE>>::emplace_back<Storm::VectorHijackerMakeBelieve>(Storm::VectorHijackerMakeBelieve hijacker)
 {
 	// We're modifying directly the size of the vector without passing by the extra initialization.
 	_Mypair._Myval2._Mylast = _Mypair._Myval2._Myfirst + hijacker._newSize;
@@ -23,9 +23,9 @@ inline decltype(auto) std::vector<STORM_HIJACKED_TYPE, std::allocator<STORM_HIJA
 
 namespace Storm
 {
-	inline void setNumUninitialized_hijack(std::vector<STORM_HIJACKED_TYPE> &hijackedVector, const Storm::VectorHijackerMakeBelieve &hijacker)
+	inline void setNumUninitialized_hijack(std::vector<STORM_HIJACKED_TYPE> &hijackedVector, Storm::VectorHijackerMakeBelieve hijacker)
 	{
-		hijackedVector.emplace_back<const Storm::VectorHijackerMakeBelieve &>(hijacker);
+		hijackedVector.emplace_back<Storm::VectorHijackerMakeBelieve>(hijacker);
 	}
 }
 #	endif
