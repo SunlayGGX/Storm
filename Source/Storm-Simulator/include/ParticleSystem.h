751f8b0fe7edabec2f57c231fa1c4fed84a7970c
SunlayGGX <sunlay.g@free.fr>
2020-11-02 10:19:51 -0500

    Made the method to compute if a particle is elligible to be a neighbor a public method.

--- Source/Storm-Simulator/include/ParticleSystem.h
+++ Source/Storm-Simulator/include/ParticleSystem.h
@@ -73,7 +73,7 @@ namespace Storm
 	public:
 		static float computeParticleDefaultVolume();
 
-	protected:
+	public:
 		static bool isElligibleNeighborParticle(const float kernelLengthSquared, const float normSquared);
 
 	public:
