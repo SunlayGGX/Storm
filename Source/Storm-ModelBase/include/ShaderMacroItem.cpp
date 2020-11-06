9f1bfe9519d01e88b7f4d0582dbc3020815cafb8
SunlayGGX <sunlay.g@free.fr>
2020-11-06 15:17:17 -0500

    Added new classes to hold shader macro definitions.

---
+++ Source/Storm-ModelBase/include/ShaderMacroItem.cpp
@@ -0,0 +1,79 @@
+#include "ShaderMacroItem.h"
+
+#include "ThrowException.h"
+
+
+Storm::ShaderMacroItem::ShaderMacroItem(const std::string_view &macroName, std::string &&macroValue) :
+	_macroName{ macroName },
+	_macroValue{ std::move(macroValue) }
+{
+	if (_macroName.empty())
+	{
+		Storm::throwException<std::exception>("Macro name shouldn't be empty!");
+	}
+}
+
+const std::string_view& Storm::ShaderMacroItem::getMacroName() const noexcept
+{
+	return _macroName;
+}
+
+const std::string& Storm::ShaderMacroItem::getMacroValue() const noexcept
+{
+	return _macroValue;
+}
+
+void Storm::ShaderMacroItem::appendToCachedName(std::string &inOutCachedNameStr) const
+{
+	inOutCachedNameStr += '_';
+	inOutCachedNameStr += this->getMacroName();
+	inOutCachedNameStr += '_';
+	inOutCachedNameStr += this->getMacroValue();
+}
+
+std::size_t Storm::ShaderMacroItem::getCacheNameStrSize() const noexcept
+{
+	return _macroName.size() + _macroValue.size() + 2;
+}
+
+Storm::ShaderMacroContainer::ShaderMacroContainer()
+{
+	_shaderMacros.reserve(2);
+}
+
+const std::vector<Storm::ShaderMacroItem>& Storm::ShaderMacroContainer::getMacros() const noexcept
+{
+	return _shaderMacros;
+}
+
+std::string Storm::ShaderMacroContainer::toCachedName() const
+{
+	class CustomShaderMacroCacheParserPolicy
+	{
+	public:
+		static std::string parsePolicyAgnostic(const std::vector<Storm::ShaderMacroItem> &macroItemContainer)
+		{
+			std::string result;
+
+			if (!macroItemContainer.empty())
+			{
+				std::size_t finalStrSize = 0;
+				for (const Storm::ShaderMacroItem &item : macroItemContainer)
+				{
+					finalStrSize += item.getCacheNameStrSize();
+				}
+
+				result.reserve(finalStrSize);
+
+				for (const Storm::ShaderMacroItem &item : macroItemContainer)
+				{
+					item.appendToCachedName(result);
+				}
+			}
+
+			return result;
+		}
+	};
+
+	return Storm::toStdString<CustomShaderMacroCacheParserPolicy>(this->getMacros());
+}
---
+++ Source/Storm-ModelBase/include/ShaderMacroItem.h
@@ -0,0 +1,52 @@
+#pragma once
+
+
+namespace Storm
+{
+	class ShaderMacroItem
+	{
+	private:
+		ShaderMacroItem(const std::string_view &macroName, std::string &&macroValue);
+
+	public:
+		template<class ValueType>
+		ShaderMacroItem(const std::string_view &macroName, ValueType &&value) :
+			Storm::ShaderMacroItem{ macroName, Storm::toStdString(std::forward<ValueType>(value)) }
+		{}
+
+	public:
+		const std::string_view& getMacroName() const noexcept;
+		const std::string& getMacroValue() const noexcept;
+
+	public:
+		// Make the cached name portion used for shader cache file name.
+		void appendToCachedName(std::string &inOutCachedNameStr) const;
+
+		std::size_t getCacheNameStrSize() const noexcept;
+
+	private:
+		std::string_view _macroName;
+		std::string _macroValue;
+	};
+
+	class ShaderMacroContainer
+	{
+	public:
+		ShaderMacroContainer();
+
+	public:
+		template<class ValueType>
+		ShaderMacroContainer& addMacro(const std::string_view &macroName, ValueType &&value)
+		{
+			_shaderMacros.emplace_back(macroName, std::forward<ValueType>(value));
+			return *this;
+		}
+
+	public:
+		const std::vector<Storm::ShaderMacroItem>& getMacros() const noexcept;
+		std::string toCachedName() const;
+
+	private:
+		std::vector<Storm::ShaderMacroItem> _shaderMacros;
+	};
+}
