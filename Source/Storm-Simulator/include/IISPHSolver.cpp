ca5bfc4f60c352ced1873d45d0be56ca422bee54
SunlayGGX <sunlay.g@free.fr>
2020-11-30 12:35:13 -0500

    Fixed IISPH from refactor. CFL remains broken.

--- Source/Storm-Simulator/include/IISPHSolver.cpp
+++ Source/Storm-Simulator/include/IISPHSolver.cpp
@@ -5,6 +5,8 @@
 #include "SingletonHolder.h"
 #include "IConfigManager.h"
 
+#include "SimulatorManager.h"
+
 #include "FluidParticleSystem.h"
 #include "RigidBodyParticleSystem.h"
 
@@ -90,6 +92,8 @@ void Storm::IISPHSolver::execute(const Storm::IterationParameter &iterationParam
 	const Storm::IConfigManager &configMgr = singletonHolder.getSingleton<Storm::IConfigManager>();
 	const Storm::GeneralSimulationData &generalSimulData = configMgr.getGeneralSimulationData();
 	const Storm::FluidData &fluidConfigData = configMgr.getFluidData();
+	
+	Storm::SimulatorManager &simulMgr = Storm::SimulatorManager::instance();
 
 	const float k_kernelZero = Storm::retrieveKernelZeroValue(generalSimulData._kernelMode);
 	const Storm::RawKernelMethodDelegate rawKernel = Storm::retrieveRawKernelMethod(generalSimulData._kernelMode);
@@ -103,7 +107,12 @@ void Storm::IISPHSolver::execute(const Storm::IterationParameter &iterationParam
 
 	Storm::ParticleSystemContainer &particleSystems = *iterationParameter._particleSystems;
 
-	// 1st : Compute the base density
+	// 1st : Initialize iteration
+	simulMgr.advanceBlowersTime(iterationParameter._deltaTime);
+	simulMgr.refreshParticleNeighborhood();
+	simulMgr.subIterationStart();
+
+	// 2nd : Compute the base density
 	for (auto &particleSystemPair : particleSystems)
 	{
 		Storm::ParticleSystem &currentParticleSystem = *particleSystemPair.second;
@@ -142,7 +151,7 @@ void Storm::IISPHSolver::execute(const Storm::IterationParameter &iterationParam
 		}
 	}
 
-	// 2nd : Compute the non pressure forces (viscosity)
+	// 3rd : Compute the non pressure forces (viscosity)
 	for (auto &particleSystemPair : particleSystems)
 	{
 		Storm::ParticleSystem &currentParticleSystem = *particleSystemPair.second;
@@ -249,7 +258,7 @@ void Storm::IISPHSolver::execute(const Storm::IterationParameter &iterationParam
 		}
 	}
 
-	// 3rd : compute advection coefficients
+	// 4th : compute advection coefficients
 	for (auto &dataFieldPair : _data)
 	{
 		// Since data field was made from fluids particles only, no need to check if this is a fluid.
@@ -348,7 +357,7 @@ void Storm::IISPHSolver::execute(const Storm::IterationParameter &iterationParam
 		});
 	}
 
-	// 4th : start prediction
+	// 5th : start prediction
 	float averageDensityError;
 
 	do 
@@ -498,7 +507,7 @@ void Storm::IISPHSolver::execute(const Storm::IterationParameter &iterationParam
 
 	this->updateCurrentPredictionIter(currentPredictionIter, generalSimulData._maxPredictIteration, averageDensityError, generalSimulData._maxDensityError, 0);
 
-	// 5th : Compute the pressure force
+	// 6th : Compute the pressure force
 	for (auto &dataFieldPair : _data)
 	{
 		// Since data field was made from fluids particles only, no need to check if this is a fluid.
@@ -573,4 +582,49 @@ void Storm::IISPHSolver::execute(const Storm::IterationParameter &iterationParam
 			tmpPressureForces[currentPIndex] = (currentPData._predictedAcceleration - currentPData._nonPressureAcceleration) * currentPMass;
 		});
 	});
+
+	// 7th : flush physics state (rigid bodies)
+	simulMgr.flushPhysics(iterationParameter._deltaTime);
+
+	// 8th : update fluid positions (Euler integration)
+	std::atomic<bool> dirtyTmp;
+	for (auto &particleSystemPair : particleSystems)
+	{
+		Storm::ParticleSystem &currentPSystem = *particleSystemPair.second;
+		if (currentPSystem.isFluids())
+		{
+			Storm::FluidParticleSystem &currentPSystemAsFluid = static_cast<Storm::FluidParticleSystem &>(currentPSystem);
+			const std::vector<float> &masses = currentPSystemAsFluid.getMasses();
+			std::vector<Storm::Vector3> &velocities = currentPSystemAsFluid.getVelocity();
+			std::vector<Storm::Vector3> &positions = currentPSystemAsFluid.getPositions();
+			const std::vector<Storm::Vector3> &force = currentPSystemAsFluid.getForces();
+
+			dirtyTmp = false;
+			constexpr const float minForceDirtyEpsilon = 0.0001f;
+
+			Storm::runParallel(force, [&](const Storm::Vector3 &currentPForce, const std::size_t currentPIndex)
+			{
+				const float &currentPMass = masses[currentPIndex];
+				Storm::Vector3 &currentPVelocity = velocities[currentPIndex];
+				Storm::Vector3 &currentPPositions = positions[currentPIndex];
+
+				currentPVelocity += currentPForce / currentPMass * iterationParameter._deltaTime;
+				currentPPositions += currentPVelocity * iterationParameter._deltaTime;
+
+				if (!dirtyTmp)
+				{
+					if (
+						std::fabs(currentPForce.x()) > minForceDirtyEpsilon ||
+						std::fabs(currentPForce.y()) > minForceDirtyEpsilon ||
+						std::fabs(currentPForce.z()) > minForceDirtyEpsilon
+						)
+					{
+						dirtyTmp = true;
+					}
+				}
+			});
+
+			currentPSystemAsFluid.setIsDirty(dirtyTmp);
+		}
+	}
 }
