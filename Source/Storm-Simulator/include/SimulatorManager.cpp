7059fda83b2cf9cbb26deabd6098284049a88227
SunlayGGX <sunlay.g@free.fr>
2020-12-16 15:47:29 -0500

    Bind the X key to the state saving feature.

--- Source/Storm-Simulator/include/SimulatorManager.cpp
+++ Source/Storm-Simulator/include/SimulatorManager.cpp
@@ -471,6 +471,7 @@ void Storm::SimulatorManager::initialize_Implementation()
 	inputMgr.bindKey(Storm::SpecialKey::KC_I, [this]() { this->resetReplay(); });
 	inputMgr.bindKey(Storm::SpecialKey::KC_C, [this]() { this->executeAllForcesCheck(); });
 	inputMgr.bindKey(Storm::SpecialKey::KC_N, [this]() { this->advanceOneFrame(); });
+	inputMgr.bindKey(Storm::SpecialKey::KC_X, [this]() { this->saveSimulationState(); });
 	inputMgr.bindKey(Storm::SpecialKey::KC_F3, [this]() { this->requestCycleColoredSetting(); });
 
 	if (configMgr.userCanModifyTimestep())
