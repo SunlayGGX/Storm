3f7f46888e2f5005313e8c5e69a8b9e612559993
SunlayGGX <sunlay.g@free.fr>
2020-12-21 11:45:22 -0500

    Redirect the lua printing output to the logging module engine.

--- Source/Storm-Script/include/LuaScriptWrapper.cpp
+++ Source/Storm-Script/include/LuaScriptWrapper.cpp
@@ -4,6 +4,66 @@
 #include "ThreadingSafety.h"
 
 
+namespace
+{
+	static int luaPrintOverride(lua_State* luaState)
+	{
+		int argsCount = lua_gettop(luaState);
+
+		if (argsCount > 0)
+		{
+			const std::string_view msgPrefix = "Lua output :\n";
+
+			std::string toPrint;
+			toPrint.reserve(msgPrefix.size() + argsCount * 32);
+
+			toPrint += msgPrefix;
+
+			for (int iter = 1; iter <= argsCount; ++iter)
+			{
+				if (lua_isstring(luaState, iter))
+				{
+					std::size_t strLen;
+					const char* msg = lua_tolstring(luaState, iter, &strLen);
+
+					toPrint.append(msg, strLen);
+				}
+				else if (lua_isboolean(luaState, iter))
+				{
+					toPrint += Storm::toStdString(lua_toboolean(luaState, iter) != 0);
+				}
+				else if (lua_isnumber(luaState, iter))
+				{
+					toPrint += Storm::toStdString(lua_tonumberx(luaState, iter, NULL));
+				}
+				else if (lua_isinteger(luaState, iter))
+				{
+					toPrint += Storm::toStdString(lua_tointegerx(luaState, iter, NULL));
+				}
+				else
+				{
+					toPrint += "Cannot print custom type (";
+					toPrint += lua_typename(luaState, iter);
+					toPrint += ")";
+				}
+
+				toPrint += "\n";
+			}
+
+			LOG_SCRIPT_LOGIC << toPrint;
+		}
+
+		return 0;
+	}
+
+	static const luaL_Reg g_redefinor[] =
+	{
+		{ "print", luaPrintOverride },
+		{ nullptr, nullptr }
+	};
+}
+
+
 Storm::LuaScriptWrapper::LuaScriptWrapper()
 {
 	_lua.open_libraries(
@@ -15,6 +75,22 @@ Storm::LuaScriptWrapper::LuaScriptWrapper()
 		sol::lib::math,
 		sol::lib::io
 	);
+
+
+	// Redirect lua output to Storm logging module.
+
+	lua_State* currentState = _lua.lua_state();
+	lua_getglobal(currentState, "_G");
+
+#define STORM_LUA_VERSION_IS_LOWER_THAN(major, minor) LUA_VERSION_NUM < major##0##minor
+#if defined(LUA_VERSION_NUM) && STORM_LUA_VERSION_IS_LOWER_THAN(5, 2)
+	luaL_register(currentState, nullptr, g_redefinor);
+#else
+	luaL_setfuncs(currentState, g_redefinor, 0);
+#endif
+#undef STORM_LUA_VERSION_IS_GREATER_THAN
+
+	lua_pop(currentState, 1);
 }
 
 bool Storm::LuaScriptWrapper::execute(const std::string &scriptContent, std::string &inOutErrorMsg)
