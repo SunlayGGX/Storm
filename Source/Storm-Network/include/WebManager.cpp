9eaabe59fc34e054704edc14ff9a44fd57c9e425
SunlayGGX <sunlay.g@free.fr>
2020-12-26 21:46:17 -0500

    Removed WebManager init and cleanUp implementations since I won't need them for now.

--- Source/Storm-Network/include/WebManager.cpp
+++ Source/Storm-Network/include/WebManager.cpp
@@ -4,12 +4,5 @@
 Storm::WebManager::WebManager() = default;
 Storm::WebManager::~WebManager() = default;
 
-void Storm::WebManager::initialize_Implementation()
-{
 
-}
 
-void Storm::WebManager::cleanUp_Implementation()
-{
-
-}
--- Source/Storm-Network/include/WebManager.h
+++ Source/Storm-Network/include/WebManager.h
@@ -3,18 +3,16 @@
 #include "Singleton.h"
 #include "IWebManager.h"
 
+#include "SingletonDefaultImplementation.h"
+
 
 namespace Storm
 {
 	class WebManager final :
-		private Storm::Singleton<Storm::WebManager>,
+		private Storm::Singleton<Storm::WebManager, Storm::DefineDefaultInitAndCleanupImplementation>,
 		public Storm::IWebManager
 	{
 		STORM_DECLARE_SINGLETON(WebManager);
-
-	private:
-		void initialize_Implementation();
-		void cleanUp_Implementation();
 
 	public:
 
