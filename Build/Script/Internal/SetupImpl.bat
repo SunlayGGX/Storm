a29368720471490a38f5a4d6f272728994533594
SunlayGGX <sunlay.g@free.fr>
2020-12-30 13:48:53 -0500

    Replaced post build step dependency copies by symlinks executed when setuping the project. Now this will be executed only once when setuping the project (dependencies aren't changed often, so it is ok to

--- Build/Script/Internal/SetupImpl.bat
+++ Build/Script/Internal/SetupImpl.bat
@@ -2,6 +2,8 @@ cd /D %~dp0
 
 SET STORM_DEPENDENCIES=%STORM_REPO_ROOT%\Dependencies
 SET STORM_BINARY=%STORM_REPO_ROOT%\bin
+SET STORM_DEBUG_BINARY=%STORM_BINARY%\Debug
+SET STORM_RELEASE_BINARY=%STORM_BINARY%\Release
 
 if exist "%STORM_DEPENDENCIES%" rmdir /s /q "%STORM_DEPENDENCIES%"
 mkdir "%STORM_DEPENDENCIES%"
@@ -13,8 +15,10 @@ if not exist "%STORM_DEPENDENCIES%" (
 
 set makeFolderLink=call :CreateFolderLink
 set createFolderIfNeeded=call :CreateFolderIfNoExist
+set provideDependencyFile=call :ProvideFileDependency
 
 
+echo Make dependency junctions
 
 :: List here the junction to be made to dependencies
 
@@ -32,16 +36,47 @@ if exist "%CATCH2_DEPENDENCIES_PATH%" (
 )
 
 
+echo Initialize the dependencies structures to the binary folders.
 
 :: Now, create the junction from the exe (where it should be) to Storm Shaders files.
 if not errorlevel 1 (
 	%createFolderIfNeeded% "%STORM_BINARY%"
-	%createFolderIfNeeded% "%STORM_BINARY%\Debug"
-	%createFolderIfNeeded% "%STORM_BINARY%\Release"
+	%createFolderIfNeeded% "%STORM_DEBUG_BINARY%"
+	%createFolderIfNeeded% "%STORM_RELEASE_BINARY%"
 
-	%makeFolderLink% "%STORM_BINARY%\Debug\Shaders" "%STORM_REPO_ROOT%\Source\Shaders"
-	%makeFolderLink% "%STORM_BINARY%\Release\Shaders" "%STORM_REPO_ROOT%\Source\Shaders"
+	%makeFolderLink% "%STORM_DEBUG_BINARY%\Shaders" "%STORM_REPO_ROOT%\Source\Shaders"
+	%makeFolderLink% "%STORM_RELEASE_BINARY%\Shaders" "%STORM_REPO_ROOT%\Source\Shaders"
 )
+
+
+set OIS_DEPENDENCY_BINARY_PATH=%STORM_DEPENDENCIES%\OIS\bin
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\OIS_d.dll" "%OIS_DEPENDENCY_BINARY_PATH%\Debug\OIS_d.dll"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\OIS_d.pdb" "%OIS_DEPENDENCY_BINARY_PATH%\Debug\OIS_d.pdb"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\OIS.dll" "%OIS_DEPENDENCY_BINARY_PATH%\Release\OIS.dll"
+
+set ASSIMP_DEPENDENCY_BINARY_PATH=%STORM_DEPENDENCIES%\Assimp\bin\code
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\assimpd.dll" "%ASSIMP_DEPENDENCY_BINARY_PATH%\Debug\assimpd.dll"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\assimpd.pdb" "%ASSIMP_DEPENDENCY_BINARY_PATH%\Debug\assimpd.pdb"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\assimp.dll" "%ASSIMP_DEPENDENCY_BINARY_PATH%\Release\assimp.dll"
+
+set PHYSX_DEPENDENCY_BINARY_PATH=%STORM_DEPENDENCIES%\PhysX\physx\lib
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysXFoundation_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysXFoundation_64.dll"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysXFoundation_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysXFoundation_64.pdb"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysX_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysX_64.dll"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysX_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysX_64.pdb"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysXCommon_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysXCommon_64.dll"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysXCommon_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysXCommon_64.pdb"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysXCooking_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysXCooking_64.dll"
+%provideDependencyFile% "%STORM_DEBUG_BINARY%\PhysXCooking_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Debug\PhysXCooking_64.pdb"
+
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysXFoundation_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysXFoundation_64.dll"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysXFoundation_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysXFoundation_64.pdb"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysX_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysX_64.dll"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysX_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysX_64.pdb"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysXCommon_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysXCommon_64.dll"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysXCommon_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysXCommon_64.pdb"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysXCooking_64.dll" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysXCooking_64.dll"
+%provideDependencyFile% "%STORM_RELEASE_BINARY%\PhysXCooking_64.pdb" "%PHYSX_DEPENDENCY_BINARY_PATH%\Release\PhysXCooking_64.pdb"
 
 
 exit /B %errorlevel%
@@ -72,3 +107,21 @@ if not errorlevel 1 (
 )
 exit /B errorlevel
 
+:: The name is like this because all we must do is provide those dependency file to a specific location.
+:: As long as the file are provided, it is unspecified if this is a copy or a junction we make under the hood.
+:ProvideFileDependency
+if not errorlevel 1 (
+	echo "%~1" to "%~2"
+	if not exist "%~2" (
+		echo Cannot create "%~1" because the target "%~2" doesn't exist. Aborting!
+		set errorlevel=90009
+	)
+	if exist "%~1" del /Q "%~1"
+	copy "%~2" "%~1"
+	if errorlevel 1 (
+		echo Failed to create junction from "%~1" to "%~2" !
+		echo.
+	)
+	echo.
+)
+exit /B errorlevel
