d2f2108d1f535b2e8df7d1cf3528c1f71e695ca7
SunlayGGX <sunlay.g@free.fr>
2021-04-07 15:08:33 -0400

    Push changes to the physX method to use when we push local forces on specific rb particles.

--- Source/Storm-Physics/include/PhysicsDynamicRigidBody.cpp
+++ Source/Storm-Physics/include/PhysicsDynamicRigidBody.cpp
@@ -124,10 +124,10 @@ void Storm::PhysicsDynamicRigidBody::resetForce()
 
 void Storm::PhysicsDynamicRigidBody::applyForce(const Storm::Vector3 &location, const Storm::Vector3 &force)
 {
-#if true
+#if false
 	physx::PxRigidBodyExt::addForceAtLocalPos(*_internalRb, Storm::convertToPx(force), Storm::convertToPx(location));
-#elif false
-	physx::PxRigidBodyExt::addLocalForceAtLocalPos(*_internalRb, Storm::convertToPx(force), Storm::convertToPx(location));	
+#elif true
+	physx::PxRigidBodyExt::addLocalForceAtLocalPos(*_internalRb, Storm::convertToPx(force), Storm::convertToPx(location));
 #else
 	const physx::PxVec3 forcePx = Storm::convertToPx(force);
 	_internalRb->addForce(forcePx);
