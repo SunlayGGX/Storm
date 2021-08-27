b96a6d22487af59fe8b2173d1695d267bbc45904
SunlayGGX <sunlay.g@free.fr>
2021-08-27 17:58:22 -0400

    Renamed variables in headers.

--- Source/Storm-Graphics/include/GraphicGravity.h
+++ Source/Storm-Graphics/include/GraphicGravity.h
@@ -9,11 +9,11 @@ namespace Storm
 	class GraphicGravity : public Storm::IRenderedElement
 	{
 	public:
-		GraphicGravity(const ComPtr<ID3D11Device> &device, const ComPtr<ID2D1RenderTarget> &renderTarget);
+		GraphicGravity(const ComPtr<ID3D11Device> &device, const ComPtr<ID2D1RenderTarget> &hudTarget);
 
 	public:
 		void render(const ComPtr<ID3D11Device> &device, const ComPtr<ID3D11DeviceContext> &deviceContext, const Storm::Camera &currentCamera) final override;
-		void postRenderUI(const ComPtr<ID2D1RenderTarget> &target, IDWriteTextFormat*const textFormat, const float viewportWidth, const float viewportHeight) final override;
+		void postRenderUI(const ComPtr<ID2D1RenderTarget> &hudTarget, IDWriteTextFormat*const textFormat, const float viewportWidth, const float viewportHeight) final override;
 
 	private:
 		void setup(const ComPtr<ID3D11DeviceContext> &deviceContext);
