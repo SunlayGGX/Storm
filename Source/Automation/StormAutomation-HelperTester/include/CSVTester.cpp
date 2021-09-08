e895aa2b2804eedbc91e77c14514a45dd64d17d6
SunlayGGX <sunlay.g@free.fr>
2021-09-08 00:39:39 -0400

    Removed deprecated ifdef.

--- Source/Automation/StormAutomation-HelperTester/include/CSVTester.cpp
+++ Source/Automation/StormAutomation-HelperTester/include/CSVTester.cpp
@@ -6,8 +6,7 @@ TEST_CASE("CSVHelpers.transcriptLetterPosition", "[classic]")
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(0) == "A");
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(2) == "C");
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(25) == "Z");
-
-#if true // Not implemented algorithm
+	
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(26) == "AA");
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(27) == "AB");
 
@@ -17,5 +16,4 @@ TEST_CASE("CSVHelpers.transcriptLetterPosition", "[classic]")
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(703) == "AAB");
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(801) == "ADV");
 	CHECK(Storm::CSVHelpers::transcriptLetterPosition(1389) == "BAL");
-#endif
 }
