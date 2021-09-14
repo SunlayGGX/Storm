093c3a7832374db1f3f6c2ca1e590de9cdfbc804
SunlayGGX <sunlay.g@free.fr>
2021-09-14 13:18:23 -0400

    Use the CSVHelper in the POC + produce the csv curves for 2 rows of neighborhood.

--- Source/POC/StormPOC-ViscosityFormula/include/StormPOC-ViscosityFormula.cpp
+++ Source/POC/StormPOC-ViscosityFormula/include/StormPOC-ViscosityFormula.cpp
@@ -5,11 +5,24 @@
 
 #include "StormPathHelper.h"
 
+#include "StormMacro.h"
+
+#include "CSVWriter.h"
+
+#include "SingletonAllocator.h"
+#include "SingletonHolder.h"
+#include "Singleton.h"
+#include "ILoggerManager.h"
+
+#include "LogHelper.h"
+#include "LogLevel.h"
+
 #include <fstream>
 #include <filesystem>
 #include <string>
 #include <cmath>
 #include <corecrt_math_defines.h>
+#include <iostream>
 
 
 #define STORM_POC_XMACRO_KERNELS																															\
@@ -20,38 +33,96 @@
 
 namespace
 {
+	using ScalarValue = double;
+
+	class LoggerManager final :
+		private Storm::Singleton<LoggerManager>,
+		public Storm::ILoggerManager
+	{
+		STORM_DECLARE_SINGLETON(LoggerManager);
+
+	public:
+		void log(const std::string_view &moduleName, Storm::LogLevel level, const std::string_view &function, const int line, std::string &&msg) final override;
+		Storm::LogLevel getLogLevel() const final override;
+		void logToTempFile(const std::string &fileName, const std::string &msg) const final override;
+	};
+
+
+	LoggerManager::LoggerManager() = default;
+	LoggerManager::~LoggerManager() = default;
+
+	void LoggerManager::log(const std::string_view & moduleName, Storm::LogLevel level, const std::string_view & function, const int line, std::string && msg)
+	{
+		const std::string_view levelStr = Storm::parseLogLevel(level);
+
+		std::string totalMsg;
+		totalMsg.reserve(16 + function.size() + moduleName.size() + msg.size() + levelStr.size());
+
+		totalMsg += "[";
+		totalMsg += levelStr;
+		totalMsg += "][";
+		totalMsg += moduleName;
+		totalMsg += "][";
+		totalMsg += function;
+		totalMsg += " (";
+		totalMsg += std::to_string(line);
+		totalMsg += ")]: ";
+		totalMsg += msg;
+		totalMsg += "\n";
+
+		std::cout << totalMsg;
+	}
+
+	Storm::LogLevel LoggerManager::getLogLevel() const
+	{
+		// No log level, we will log everything.
+		return Storm::LogLevel::Debug;
+	}
+
+	void LoggerManager::logToTempFile(const std::string & fileName, const std::string & msg) const
+	{
+		STORM_NOT_IMPLEMENTED;
+	}
+
+	using SingletonAllocatorAlias = Storm::SingletonAllocator<
+		Storm::SingletonHolder,
+		LoggerManager
+	>;
+
+	SingletonAllocatorAlias g_singletonMaker;
+
 	struct Vector
 	{
 	public:
-		float normSquared() const noexcept
+		ScalarValue normSquared() const noexcept
 		{
 			return _x * _x + _y * _y + _z * _z;
 		}
 
-		float norm() const noexcept
+		ScalarValue norm() const noexcept
 		{
-			return std::sqrtf(this->normSquared());
+			return std::sqrt(this->normSquared());
 		}
 
 	public:
-		float dot(const Vector &vect) const noexcept
+		ScalarValue dot(const Vector &vect) const noexcept
 		{
 			return _x * vect._x + _y * vect._y + _z * vect._z;
 		}
 
 	public:
-		float _x = 0.f;
-		float _y = 0.f;
-		float _z = 0.f;
+		ScalarValue _x = 0.f;
+		ScalarValue _y = 0.f;
+		ScalarValue _z = 0.f;
 	};
 
 
-	Vector operator*(const float coeff, const Vector &other) noexcept
+	Vector operator*(const ScalarValue coeff, const Vector &other) noexcept
 	{
 		return Vector{ other._x * coeff, other._y * coeff, other._z * coeff };
 	}
 
-	Vector operator*(const Vector &other, const float coeff) noexcept
+	Vector operator*(const Vector &other, const ScalarValue coeff) noexcept
 	{
 		return coeff * other;
 	}
@@ -71,7 +142,7 @@ namespace
 	class Kernel
 	{
 	public:
-		Kernel(float h, KernelFunc func) :
+		Kernel(ScalarValue h, KernelFunc func) :
 			_h{ h },
 			_hSquared{ h * h },
 			_func{ func }
@@ -88,7 +159,7 @@ namespace
 		}
 
 	public:
-		Vector gradient(const Vector &xji, const float norm) const
+		Vector gradient(const Vector &xji, const ScalarValue norm) const
 		{
 			switch (_func)
 			{
@@ -102,35 +173,35 @@ namespace
 		}
 
 	private:
-		float computeSplishSplashCubicSplinePrecoeff() const noexcept
+		ScalarValue computeSplishSplashCubicSplinePrecoeff() const noexcept
 		{
-			constexpr float k_constexprGradientPrecoeffCoeff = static_cast<float>(48.0 / M_PI);
+			constexpr ScalarValue k_constexprGradientPrecoeffCoeff = static_cast<ScalarValue>(48.0 / M_PI);
 			return k_constexprGradientPrecoeffCoeff / (_hSquared * _hSquared);
 		}
 
-		float computeJJMonaghanCubicSplinePrecoeff() const noexcept
+		ScalarValue computeJJMonaghanCubicSplinePrecoeff() const noexcept
 		{
-			constexpr float k_constexprGradientPrecoeffCoeff = static_cast<float>(-3.0 / 4.0 / M_PI);
+			constexpr ScalarValue k_constexprGradientPrecoeffCoeff = static_cast<ScalarValue>(-3.0 / 4.0 / M_PI);
 			return k_constexprGradientPrecoeffCoeff / (_hSquared * _hSquared);
 		}
 
-		constexpr float computeJJMonaghanCubicSplinePrecoeffClamped() const noexcept
+		constexpr ScalarValue computeJJMonaghanCubicSplinePrecoeffClamped() const noexcept
 		{
 			// 0.02 / 2 * cs² where cs is the maximum speed of a particle. Since we're making and controlling the data ourself, we know the maximum speed is 1.0 (vj speed).
 			// The division by 2 is because I simplified the kernel by removing a 1/2 and passing it to the precoeff instead.
-			constexpr float k_constexprGradientPrecoeffCoeff = static_cast<float>(0.01 * 1.0 * 1.0);
+			constexpr ScalarValue k_constexprGradientPrecoeffCoeff = static_cast<ScalarValue>(0.01 * 1.0 * 1.0);
 			return k_constexprGradientPrecoeffCoeff;
 		}
 
 	private:
-		Vector gradientSplishSplashCubicSpline(const Vector &xji, const float norm) const
+		Vector gradientSplishSplashCubicSpline(const Vector &xji, const ScalarValue norm) const
 		{
 			Vector result;
 
 			if (norm <= _h)
 			{
-				const float q = norm / _h;
-				float factor;
+				const ScalarValue q = norm / _h;
+				ScalarValue factor;
 
 				if (q < 0.5f)
 				{
@@ -138,7 +209,7 @@ namespace
 				}
 				else
 				{
-					const float subfactor = 1.f - q;
+					const ScalarValue subfactor = 1.f - q;
 					factor = -subfactor * subfactor;
 				}
 
@@ -148,7 +219,7 @@ namespace
 			return result;
 		}
 
-		Vector gradientJJMonaghanCubicSpline(const Vector &xji, const float norm) const
+		Vector gradientJJMonaghanCubicSpline(const Vector &xji, const ScalarValue norm) const
 		{
 			Vector result;
 
@@ -160,8 +231,8 @@ namespace
 				// Then with a variable change (or variable substitution) :
 				// JJk = _h / 2
 				// q = norm / JJk	=>	q = norm / (_h / 2)		=>	q = norm * 2 / _h
-				const float q = norm * 2.f / _h;
-				float factor;
+				const ScalarValue q = norm * 2.f / _h;
+				ScalarValue factor;
 
 				if (q < 1.f)
 				{
@@ -169,7 +240,7 @@ namespace
 				}
 				else /*if (q < 2.f)*/ // q would always be under 2.f because it is per construction from the variable change
 				{
-					const float twoMinusQ = 2.f - q;
+					const ScalarValue twoMinusQ = 2.f - q;
 					factor = twoMinusQ * twoMinusQ;
 				}
 
@@ -180,7 +251,7 @@ namespace
 		}
 
 
-		Vector gradientJJMonaghanCubicSplineClamped(const Vector &xji, const float norm) const
+		Vector gradientJJMonaghanCubicSplineClamped(const Vector &xji, const ScalarValue norm) const
 		{
 			Vector result;
 
@@ -192,10 +263,10 @@ namespace
 				// Then with a variable change (or variable substitution) :
 				// JJk = _h / 2
 				// q = norm / JJk	=>	q = norm / (_h / 2)		=>	q = norm * 2 / _h
-				const float q = norm * 2.f / _h;
-				float factor;
+				const ScalarValue q = norm * 2.f / _h;
+				ScalarValue factor;
 
-				constexpr float twoThird = 2.f / 3.f;
+				constexpr ScalarValue twoThird = 2.f / 3.f;
 
 				if (q < twoThird)
 				{
@@ -208,7 +279,7 @@ namespace
 				}
 				else /*if (q < 2.f)*/ // q would always be under 2.f because it is per construction from the variable change
 				{
-					const float twoMinusQ = 2.f - q;
+					const ScalarValue twoMinusQ = 2.f - q;
 					factor = twoMinusQ * twoMinusQ;
 				}
 
@@ -219,17 +290,17 @@ namespace
 		}
 
 	public:
-		float _h;
-		float _hSquared;
+		ScalarValue _h;
+		ScalarValue _hSquared;
 
-		float _gradientCoeff;
+		ScalarValue _gradientCoeff;
 		const KernelFunc _func;
 	};
 
 	class Particle
 	{
 	public:
-		Particle(float x, float y, float z) :
+		Particle(ScalarValue x, ScalarValue y, ScalarValue z) :
 			_position{ x, y, z }
 		{}
 
@@ -238,54 +309,26 @@ namespace
 		Vector _velocity;
 	};
 
+	struct CSVConstants
+	{
+	public:
+		inline static const std::string k_xijNormStr = "r";
+		inline static const std::string k_anglesStr = "angles";
+		inline static const std::string k_xjStr = "xj";
+		inline static const std::string k_dotCoeffStr = "dotCoeff";
+		inline static const std::string k_normStr = "norm";
+		inline static const std::string k_kernelGradStr = "kernelGrad";
+	};
+
 	class Viscosity
 	{
 	public:
-		Viscosity(const std::filesystem::path &csvFileName, float h, KernelFunc func) :
+		Viscosity(const std::filesystem::path &csvFileName, ScalarValue h, KernelFunc func) :
 			_kernel{ h, func },
-			_csvFile{ csvFileName }
+			_csv{ csvFileName.string() },
+			_monaghanStdOffset{ 0.01f * h * h }
 		{
-			Viscosity::reserveCsvData(_rValues, "r");
-			Viscosity::reserveCsvData(_normValues, "norm");
-			Viscosity::reserveCsvData(_angleValues, "angles");
-			Viscosity::reserveCsvData(_normCoeffValues, "dotCoeff");
-			Viscosity::reserveCsvData(_xjValues, "xj");
-			Viscosity::reserveCsvData(_kernelValues, "kernelGrad");
-		}
 
-		~Viscosity()
-		{
-			this->writeCsvData(_rValues);
-			this->writeCsvData(_xjValues);
-			this->writeCsvData(_angleValues);
-			this->writeCsvData(_normValues);
-			this->writeCsvData(_normCoeffValues);
-			this->writeCsvData(_kernelValues);
-		}
-
-	public:
-		__forceinline static void appendCsvData(std::string &inOutCsvLine, const float value)
-		{
-			inOutCsvLine += std::to_string(value);
-			inOutCsvLine += ',';
-		}
-
-		static void reserveCsvData(std::string &inOutCsvLine, const std::string_view &dataName)
-		{
-			inOutCsvLine.reserve(256 + dataName.size());
-			inOutCsvLine += dataName;
-			inOutCsvLine += ',';
-		}
-
-		void writeCsvData(std::string &inOutCsvLine)
-		{
-			// Remove trailing commas if any
-			while (inOutCsvLine.back() == ',')
-			{
-				inOutCsvLine.pop_back();
-			}
-
-			_csvFile << inOutCsvLine << '\n';
 		}
 
 	public:
@@ -294,11 +337,11 @@ namespace
 			const Vector xij = pi._position - pj._position;
 			const Vector vij = pi._velocity - pj._velocity;
 
-			const float xijNormSquared = xij.normSquared();
-			const float xijNorm = xij.norm();
-			const float vijDotXij = vij.dot(xij);
+			const ScalarValue xijNormSquared = xij.normSquared();
+			const ScalarValue xijNorm = xij.norm();
+			const ScalarValue vijDotXij = vij.dot(xij);
 
-			const float dotCoeff = vijDotXij / (xijNormSquared + 0.01f * _kernel._hSquared);
+			const ScalarValue dotCoeff = vijDotXij / (xijNormSquared + 0.01f * _kernel._hSquared);
 
 			const Vector gradValue = _kernel.gradient(-1.f * xij, xijNorm);
 
@@ -306,29 +349,23 @@ namespace
 			// The real value I want a graph of is the part of the formula with the kernel and the dot product.
 			const Vector forceApprox = gradValue * dotCoeff;
 
-			const float forceNorm = forceApprox.norm();
+			const ScalarValue forceNorm = 10.f * forceApprox.norm() / (xijNormSquared + _monaghanStdOffset);
 
-			constexpr float radToDegreeCoeff = static_cast<float>(180.0 / M_PI);
+			constexpr ScalarValue radToDegreeCoeff = static_cast<ScalarValue>(180.0 / M_PI);
 
-			Viscosity::appendCsvData(_rValues, xijNorm);
-			Viscosity::appendCsvData(_angleValues, std::sinf(pj._position._x / xijNorm) * radToDegreeCoeff);
-			Viscosity::appendCsvData(_xjValues, pj._position._x);
-			Viscosity::appendCsvData(_normCoeffValues, dotCoeff);
-			Viscosity::appendCsvData(_normValues, forceNorm);
-			Viscosity::appendCsvData(_kernelValues, gradValue.norm());
+			_csv(CSVConstants::k_xijNormStr, xijNorm);
+			_csv(CSVConstants::k_anglesStr, std::sin(pj._position._x / xijNorm) * radToDegreeCoeff);
+			_csv(CSVConstants::k_xjStr, pj._position._x);
+			_csv(CSVConstants::k_dotCoeffStr, dotCoeff);
+			_csv(CSVConstants::k_normStr, forceNorm);
+			_csv(CSVConstants::k_kernelGradStr, gradValue.norm());
 		}
 
 	public:
 		const Kernel _kernel;
 
-		std::string _rValues;
-		std::string _angleValues;
-		std::string _normValues;
-		std::string _normCoeffValues;
-		std::string _xjValues;
-		std::string _kernelValues;
-
-		std::ofstream _csvFile;
+		Storm::CSVWriter _csv;
+		const ScalarValue _monaghanStdOffset;
 	};
 
 	std::filesystem::path initPOC(int argc, char* argv[])
@@ -347,31 +384,38 @@ namespace
 		return currentTempPath;
 	}
 
-	void exec(const std::filesystem::path &tempFolderPathToWriteResult, const std::string_view &csvName, const KernelFunc func)
+	void exec(const std::filesystem::path &tempFolderPathToWriteResult, const std::string &csvName, const KernelFunc func, unsigned int layer)
 	{
+		constexpr ScalarValue h = 5.f;
+		constexpr ScalarValue particleRadius = h / 4.5f;
+
+		assert(layer < 2 && "If layer is equal or greater than 2, particle won't be in the influence radius if the kernel.");
+
 		// Radius of r=5m
-		Viscosity viscosity{ tempFolderPathToWriteResult / csvName, 5.f, func };
+		Viscosity viscosity{ tempFolderPathToWriteResult / csvName, h, func };
 
 		// Origin particle set at { 0.f, 0.f, 0.f }
-		Particle pi{ 0.f, 0.f, 0.f };
+		Particle pi{ 0.0, 0.0, 0.0 };
 
 		// Moving particle set on the circle of r=5m.
 		// The equation of a circle is x² + y² + z² = r²...
 		// The max x is when y = 0 & z = 0 => x²=r² => x=r => x=5,
 		// therefore, I can initialize the position of pj with x C [-5, 5].
-		// I choose x=4.75m and z=0m => x²+y²+z²=r² => x²+y²+0²=5² => y²=25-x²-0 => y=sqrt(25-x²) or y=-sqrt(25-x²), I choose the positive one
-		constexpr float chosenXStart = 4.75f;
-		constexpr float chosenXEnd = -chosenXStart;
-		Particle pj{ chosenXStart, std::sqrtf(25.f - (chosenXStart * chosenXStart)), 0.f };
+		// I choose y = (layer + 1) * 2 * particleRadius, and z=0m => x²+y²+z²=r² => x²=25-y²-0 => x_max=sqrt(25 - ((layer + 1) * 2 * particleRadius)²) and x_min=-x_max.
+		const ScalarValue y = static_cast<ScalarValue>(layer + 1) * (2.f * particleRadius);
+		const ScalarValue xMax = std::sqrt(25.0 - y * y);
+		const ScalarValue xMin = -xMax;
+
+		Particle pj{ xMin, y, 0.0 };
 
 		// Pj will have a velocity of { -1.f, 0.f, 0.f }, therefore will pass parallel to the x axis.
-		pj._velocity = Vector{ -1.f, 0.f, 0.f };
+		pj._velocity = Vector{ 1.0, 0.0, 0.0 };
 
-		const float _xStep = 0.001f;
-		while (pj._position._x > chosenXEnd)
+		const ScalarValue _xStep = 0.0005f;
+		while (pj._position._x < xMax)
 		{
 			viscosity(pi, pj);
-			pj._position._x -= _xStep;
+			pj._position._x += _xStep;
 		}
 	}
 }
@@ -380,9 +424,12 @@ int main(int argc, char* argv[])
 {
 	const std::filesystem::path tempFolderPathToWriteResult = initPOC(argc, argv);
 
-#define STORM_POC_XMACRO_KERNEL_ELEM(Mode, Func, PrecoeffInit) exec(tempFolderPathToWriteResult, "visco" #Mode ".csv", KernelFunc::Mode);
-	STORM_POC_XMACRO_KERNELS
+	for (unsigned int iter = 0; iter < 3; ++iter)
+	{
+#define STORM_POC_XMACRO_KERNEL_ELEM(Mode, Func, PrecoeffInit) exec(tempFolderPathToWriteResult, "visco" #Mode "_" + std::to_string(iter) + ".csv", KernelFunc::Mode, iter);
+		STORM_POC_XMACRO_KERNELS
 #undef STORM_POC_XMACRO_KERNEL_ELEM
+	}
 
 	return 0;
 }
