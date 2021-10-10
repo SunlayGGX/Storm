1a7312697b0f9ad0dd005e29430effe0a34ac5e2
SunlayGGX <sunlay.g@free.fr>
2021-10-10 11:15:11 -0400

    Made compliant VS parser with methods only, just to print the fileline and filepath in a compliant way.

---
+++ Source/Storm-Helper/include/VisualStudioOutputCompliantParser.cpp
@@ -0,0 +1,20 @@
+#include "VisualStudioOutputCompliantParser.h"
+
+
+void Storm::VisualStudioOutputCompliantParser::produceVSOutputCompliantLine(std::string &inOutAppend, const std::string_view filePath, const std::size_t line)
+{
+	if (!filePath.empty())
+	{
+		inOutAppend += filePath;
+		inOutAppend += '(';
+		inOutAppend += std::to_string(line);
+		inOutAppend += ')';
+	}
+}
+
+void Storm::VisualStudioOutputCompliantParser::produceVSOutputCompliantLine(std::string &inOutAppend, const std::size_t preNumber, const std::string_view filePath, const std::size_t line)
+{
+	inOutAppend += std::to_string(preNumber);
+	inOutAppend += '>';
+	Storm::VisualStudioOutputCompliantParser::produceVSOutputCompliantLine(inOutAppend, filePath, line);
+}
