c06f6b530a68c0eed93855f164c2f757654905af
SunlayGGX <sunlay.g@free.fr>
2022-11-23 23:09:13 -0500

    made NameExtractor a consteval method.

--- Source/Storm-Helper/include/NameExtractor.h
+++ Source/Storm-Helper/include/NameExtractor.h
@@ -9,7 +9,7 @@ namespace Storm
 	{
 	public:
 		// Not from method... like it doesn't work for __FUNCTION__ because I'm lazy to make an algorithm for something I don't need. YAGNI...
-		static constexpr std::string_view extractTypeNameFromType(const std::string_view &fullName)
+		static consteval std::string_view extractTypeNameFromType(const std::string_view &fullName)
 		{
 			std::size_t posLastDot = fullName.find_last_of(':');
 			return fullName.substr(posLastDot == std::string_view::npos ? 0 : posLastDot + 1);
